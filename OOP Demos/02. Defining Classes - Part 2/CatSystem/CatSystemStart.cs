6ab421bd798fefdc011ef9cffbc44d654ddd90fc
petyakostova <petya_kostova_@abv.bg>
2015-03-22 19:09:50 +0200

    added static class Printer.cs in CatSystem.csproj and use it

--- OOP Demos/02. Defining Classes - Part 2/CatSystem/CatSystemStart.cs
+++ OOP Demos/02. Defining Classes - Part 2/CatSystem/CatSystemStart.cs
@@ -1,6 +1,7 @@
 ﻿namespace CatSystem
 {
     using System;
+    using System.Collections.Generic;
 
     public class CatSystemStart
     {
@@ -24,15 +25,20 @@
             var anotherCat = new Cat(CatColor.Black);
             var yetAnotherCat = new Cat(CatColor.Brown);
 
-            peshoOwner.AddCat(cat, "Fluffy");
+            peshoOwner.AddCat(cat, "Dzhinks");
             peshoOwner.AddCat(anotherCat, "Silvestar");
 
             // this will cause Unhandled Exception: System.NullReferenceException: Object reference not set to an instance of an object. => Line 25 in Owner.cs
             Console.WriteLine(peshoOwner.AllCats);
 
+            // print static property
             Console.WriteLine(Cat.NumberOfLegs);
 
+            // print the result from a static method WhatDoesTheCatSay()
             Console.WriteLine(Cat.WhatDoesTheCatSay());
+
+            // invoke the static method PrintCat() in static class Printer
+            Printer.PrintCat(cat);
         }
     }
 }
---
+++ OOP Demos/02. Defining Classes - Part 2/CatSystem/Printer.cs
@@ -0,0 +1,11 @@
+﻿namespace CatSystem
+{
+    // static class
+    public static class Printer
+    {
+        public static void PrintCat(Cat cat)
+        {
+            System.Console.WriteLine(cat.Name + " " + cat.Аgе);
+        }
+    }
+}
