09ae9fa58709a4934c8ba5c2c1bb56e515edd277
petyakostova <petya_kostova_@abv.bg>
2015-03-28 20:26:20 +0200

    modified Students.csproj

--- OOP Demos/04. OOP Principles - Part 1/Students/StudentsMain.cs
+++ OOP Demos/04. OOP Principles - Part 1/Students/StudentsMain.cs
@@ -1,6 +1,7 @@
 ﻿namespace Students
 {
     using System;
+    using System.Collections.Generic;
 
     class StudentsMain
     {
@@ -9,6 +10,19 @@
             JuniorStudent jns = new JuniorStudent();
             jns.Name = "Ivan";
             Console.WriteLine(jns.GetAllCourses());
+
+            IEnumerable<string> enumerable = new List<string>(); //using System.Collections.Generic;
+            IEnumerable<string> hashset = new HashSet<string>();      //using System.Collections.Generic;
+            // List and HashSet can be foreach... because there are from same interface
+
+            ISet<string> set = new SortedSet<string>();     
+            ISet<string> anotherSet = new HashSet<string>();
+            // SortedSet and HashSet implement ISet<T> and have same methods
         }
+
+        //static void Print(ISet<string> set)
+        //{
+        //    return; 
+        //}
     }
 }
