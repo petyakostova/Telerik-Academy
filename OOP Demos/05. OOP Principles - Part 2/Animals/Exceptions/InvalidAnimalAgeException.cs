82e83d23d73eb93befc5f31eda36759829835851
petyakostova <petya_kostova_@abv.bg>
2015-04-02 22:46:47 +0300

    added OOP Demos/05. OOP Principles - Part 2/Animals/Exceptions/InvalidAnimalAgeException.cs

---
+++ OOP Demos/05. OOP Principles - Part 2/Animals/Exceptions/InvalidAnimalAgeException.cs
@@ -0,0 +1,18 @@
+﻿namespace Animals.Exceptions
+{
+    using System;
+
+    public class InvalidAnimalAgeException : ApplicationException
+    {
+        public InvalidAnimalAgeException(string message, int minAge, int maxAge)
+            : base(message) // invokes the base constructor - every exception can accept message
+        {
+            this.MinAge = minAge;
+            this.MaxAge = maxAge;
+        }
+
+        public int MinAge { get; private set; }
+
+        public int MaxAge { get; private set; }
+    }
+}
