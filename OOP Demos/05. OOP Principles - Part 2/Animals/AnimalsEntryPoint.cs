f66805a99568c4996ec4cd0fcede393acdfdc348
petyakostova <petya_kostova_@abv.bg>
2015-04-02 04:30:17 +0300

    describing the uses of the polymorphism

--- OOP Demos/05. OOP Principles - Part 2/Animals/AnimalsEntryPoint.cs
+++ OOP Demos/05. OOP Principles - Part 2/Animals/AnimalsEntryPoint.cs
@@ -6,7 +6,7 @@
 
     public class AnimalsEntryPoint
     {
-        // using polymorphism
+        // using polymorphism - pass more specific object ro a method that expects a parameter of more generic type
         public static void SayAnimalName(IAnimal animal)
         {
             //// this is anti QPC => Don't do it!
@@ -66,7 +66,7 @@
 
             Console.WriteLine();
 
-            // using polymorphism
+            // using polymorphism - mix different related types in the same collection
             List<IAnimal> animals = new List<IAnimal>();
             animals.Add(cat);
             animals.Add(secondCat);
@@ -82,13 +82,28 @@
 
             Console.WriteLine();
 
-            // using polymorphism
+            // using polymorphism - invoke abstract operations
             SayAnimalName(cat);
             SayAnimalName(secondCat);
             SayAnimalName(thirdCat);
             SayAnimalName(dog);
             SayAnimalName(secondDog);
             SayAnimalName(thirdDog);
+
+            Console.WriteLine();
+
+            // using polymorphism - declare a more generic field which will be initialized and "specialized" later
+            IAnimal animal;
+            string animalType = "Cat";
+            switch (animalType)
+            {
+                case "Cat": { animal = new Cat("Puh"); Console.WriteLine(animal.Name); break; }
+                case "Dog": { animal = new Dog("Snoop"); Console.WriteLine(animal.Name); break; }
+                default:
+                    break;
+            }
+
+            
         }
     }
 }
