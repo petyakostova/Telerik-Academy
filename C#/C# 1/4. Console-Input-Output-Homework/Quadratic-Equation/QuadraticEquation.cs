2f3bc2616779dd0fd97239a8066234468824b728
petyakostova <petya_kostova_@abv.bg>
2016-03-31 22:11:11 +0300

    modify Quadratic-Equation/QuadraticEquation.cs added README.md

--- C#/C# 1/4. Console-Input-Output-Homework/Quadratic-Equation/QuadraticEquation.cs
+++ C#/C# 1/4. Console-Input-Output-Homework/Quadratic-Equation/QuadraticEquation.cs
@@ -1,60 +1,53 @@
 ﻿/* Problem 6. Quadratic Equation
-Write a program that reads the coefficients a, b and c of a quadratic equation ax2 + bx + c = 0 
-and solves it (prints its real roots).
-Examples:
-a 	    b 	c 	    roots
-2 	    5 	-3 	    x1=-3; x2=0.5
--1 	    3 	0 	    x1=3; x2=0
--0.5    4 	-8 	    x1=x2=4
-5 	    2 	8 	    no real roots
-*/
+   Write a program that reads the coefficients a, b and c of a quadratic equation 
+   ax2 + bx + c = 0 and solves it (prints its real roots). 
+   On the first three lines, you will receive the coefficients a, b, and c, each on a separate line in the same order
+   Output:
+    If two different real roots exist, print them on two separate lines. Print the smaller root on the first line
+    If only one real root exists, print it on the only output line.
+    If no real root exists, print the string "no real roots".
+    The roots, should they exist, must be printed with precision exactly two digits after the floating point
+   Constraints: The input will always consist of valid real numbers in the rande [-1000, 1000] and will follow the described format*/
 
 using System;
-using System.Threading;                                                         // line 13 and 14 are necessary for the line 20
+using System.Threading;                                                         
 using System.Globalization;
 
 class QuadraticEquation
 {
     static void Main()
     {
-        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;     /* changes the culture of the program in American standard
-                                                                                 * so the decimal point character is . */
-        double a, b, c;
-        Console.Write("Enter the coefficient a= ");
-        while (!double.TryParse(Console.ReadLine(), out a))                     // parsing and input check (validating the user data)
-        {
-            Console.WriteLine("Invalid number.");
-            Console.Write("Please enter a correct coefficient a= ");
-        }
-        Console.Write("Enter the coefficient b= ");
-        while (!double.TryParse(Console.ReadLine(), out b))                     // parsing and input check
-        {
-            Console.WriteLine("Invalid number.");
-            Console.Write("Please enter a correct coefficient b= ");
-        }
-        Console.Write("Enter the coefficient c= ");
-        while (!double.TryParse(Console.ReadLine(), out c))                    // parsing and input check
-        {
-            Console.WriteLine("Invalid number.");
-            Console.Write("Please enter a correct coefficient c= ");
-        }
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c = double.Parse(Console.ReadLine());
+        
         double x1, x2;
-        if (b * b - 4 * a * c < 0)                                              // if (Discriminant < 0)
+                
+        if (b * b - 4 * a * c < 0) // if (Discriminant < 0)                                             
         {
             Console.WriteLine("no real roots");
         }
         else
-        {
-            if (b * b - 4 * a * c == 0)                                         // if (Discriminant == 0)
+        {            
+            if (b * b - 4 * a * c == 0) // if (Discriminant == 0)                                         
             {
                 x1 = x2 = -b / (2 * a);
-                Console.WriteLine("x1 = x2 = {0}", x1);
+                Console.WriteLine("{0:F2}", x1);
             }
-            else                                                                // if (Discriminant > 0)
+            else // if (Discriminant > 0)                                                               
             {
                 x1 = (-b - Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
                 x2 = (-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
-                Console.WriteLine("x1 = {0};    x2 = {1}", x1, x2);
+                if (x1 < x2)
+                {
+                    Console.WriteLine("{0:F2}\r\n{1:F2}", x1, x2);
+                }
+                else
+                {
+                    Console.WriteLine("{0:F2}\r\n{1:F2}", x2, x1);
+                }
             }
         }
     }
---
+++ C#/C# 1/4. Console-Input-Output-Homework/Quadratic-Equation/README.md
@@ -0,0 +1,31 @@
+# Quadratic Equation
+
+## Description
+Write a program that reads the coefficients **a**, **b** and **c** of a quadratic equation *ax<sup>2</sup> + bx + c = 0* and solves it (prints its real roots).
+
+## Input
+- On the first three lines, you will receive the coefficients **a**, **b**, and **c**, each on a separate line in the same order
+
+## Output
+- If two different real roots exist, print them on two separate lines
+  - Print the smaller root on the first line
+- If only one real root exists, print it on the only output line
+- If no real root exists, print the string "no real roots"
+- **The roots, should they exist, must be printed with precision _exactly_ two digits after the floating point**
+
+## Constraints
+- The input will always consist of valid real numbers in the rande [-1000, 1000] and will follow the described format
+- Time limit: **0.1s**
+- Memory limit: **8MB**
+
+## Sample tests
+
+|     Input       |     Output     |
+|:---------------:|:--------------:|
+|2<br/>5<br/>-3   |-3.00<br/>0.50  |
+|-1<br/>3<br/>0   |0.00<br/>3.00   |
+|-0.5<br/>4<br/>-8|4.00            |
+|5<br/>2<br/>8    |no real roots   |
+
+## Submission
+- Submit your code [here](http://bgcoder.com/Contests/Compete/Index/311#4)
