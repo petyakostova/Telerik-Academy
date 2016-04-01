6bc905b893001cf98fccb751c058e1587e3395d6
petyakostova <petya_kostova_@abv.bg>
2016-04-02 00:57:40 +0300

    solve Zero-Subset; update README.md

--- C#/C# 1/5. Conditional-Statements-Homework/Zero-Subset/README.md
+++ C#/C# 1/5. Conditional-Statements-Homework/Zero-Subset/README.md
@@ -1,7 +1,9 @@
 # *Zero Subset
 
 ## Description
-We are given 5 integer numbers. Write a program that finds all subsets of these numbers whose sum is 0.
+We are given 5 integer numbers in a single line, separated by a space. 
+
+Write a program that finds all subsets of these numbers whose sum is 0.
 
 Assume that repeating the same subset several times is not a problem.
 
--- C#/C# 1/5. Conditional-Statements-Homework/Zero-Subset/ZeroSubset.cs
+++ C#/C# 1/5. Conditional-Statements-Homework/Zero-Subset/ZeroSubset.cs
@@ -1,5 +1,5 @@
 ﻿/* Problem 12.* Zero Subset
-   We are given 5 integer numbers. 
+   We are given 5 integer numbers in a single line, separated by a space. 
    Write a program that finds all subsets of these numbers whose sum is 0.
    Assume that repeating the same subset several times is not a problem.
    Hint: you may check for zero each of the 31 subsets with 31 if statements. */
@@ -10,11 +10,27 @@ class ZeroSubset
 {
     static void Main()
     {
-        int a = int.Parse(Console.ReadLine());
-        int b = int.Parse(Console.ReadLine());
-        int c = int.Parse(Console.ReadLine());
-        int d = int.Parse(Console.ReadLine());
-        int e = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter 5 integer numbers in a single line, separated by a space: ");
+        string line = Console.ReadLine();
+        // define array of characters-separators ' '    [can be also { ' ', ',', '.' } - for space, comma and dot]
+        char[] separators = new char[] { ' ' };
+        /* define array of strings and using the built-in functionality of the method Split(…) from the class String, 
+         * split the contents of a given Input (which is String) by array of characters–separators, 
+           which are passed as an argument of the method. 
+           All substrings among which is space will be removed and stored in the splitNumbers array.
+         * For removing the empty elements after splitting, we add as a second parameter (argument) the constant 
+           StringSplitOptions.RemoveEmptyEntries         so we instruct the method Split(…) to: 
+         * Return all substrings from the variable that are split by given list of separators. 
+         * Consider two or more neighboring separators as one. */
+        string[] splitNumbers = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+        //string[] splitNumbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        int a = int.Parse(splitNumbers[0]);
+        int b = int.Parse(splitNumbers[1]);
+        int c = int.Parse(splitNumbers[2]);
+        int d = int.Parse(splitNumbers[3]);
+        int e = int.Parse(splitNumbers[4]);
 
         int count = 0;
 
