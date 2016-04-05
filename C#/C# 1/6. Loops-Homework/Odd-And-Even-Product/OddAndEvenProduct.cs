c309cc8260dd4cf5707bbbd5bc487cd74182d6dc
petyakostova <petya_kostova_@abv.bg>
2016-04-05 19:26:04 +0300

    modify Odd-And-Even-Product/OddAndEvenProduct.cs

--- C#/C# 1/6. Loops-Homework/Odd-And-Even-Product/OddAndEvenProduct.cs
+++ C#/C# 1/6. Loops-Homework/Odd-And-Even-Product/OddAndEvenProduct.cs
@@ -19,43 +19,29 @@ class OddAndEvenProduct
 {
     static void Main()
     {
-        //Console.WriteLine("Enter integer numbers given in a single line, separated by a space: ");
-        string line = Console.ReadLine();
-        // define array of characters-separators ' ' [can be also { ' ', ',', '.' } - for space, comma and dot]
-        char[] separators = new char[] { ' ' };
-        /* define array of strings and using the built-in functionality of the method Split(…) from the class String, 
-         * split the contents of a given Input (which is String) by array of characters–separators, 
-           which are passed as an argument of the method. 
-           All substrings among which is space will be removed and stored in the splitNumbers array.
-         * For removing the empty elements after splitting, we add as a second parameter (argument) the constant 
-           StringSplitOptions.RemoveEmptyEntries         so we instruct the method Split(…) to: 
-         * Return all substrings from the variable that are split by given list of separators. 
-         * Consider two or more neighboring separators as one. */
-        string[] splitNumbers = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        int n = int.Parse(Console.ReadLine());
+        string[] splitNumbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
         BigInteger oddProduct = 1;
         BigInteger evenProduct = 1;
 
-        for (int i = 0; i < splitNumbers.Length; i += 2)
+        for (int i = 0; i < n; i += 2)    // i < splitNumbers.Length
         {
             oddProduct *= int.Parse(splitNumbers[i]);
         }
 
-        for (int i = 1; i < splitNumbers.Length; i += 2)
+        for (int i = 1; i < n; i += 2)    // i < splitNumbers.Length
         {
             evenProduct *= int.Parse(splitNumbers[i]);
         }
 
         if (oddProduct == evenProduct)
         {
-            Console.WriteLine("yes");
-            Console.WriteLine("product = " + oddProduct);
+            Console.WriteLine("yes {0}", oddProduct);
         }
         else
         {
-            Console.WriteLine("no");
-            Console.WriteLine("odd_product = " + oddProduct);
-            Console.WriteLine("even_product = " + evenProduct);
+            Console.WriteLine("no {0} {1}", oddProduct, evenProduct);
         }
 
     }
