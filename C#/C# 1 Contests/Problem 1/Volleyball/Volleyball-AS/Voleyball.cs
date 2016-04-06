834c7b444567f3b13e515d9916430f531e4fd35c
petyakostova <petya_kostova_@abv.bg>
2016-04-06 10:55:32 +0300

    add C# 1 Contest => Volleyball

---
+++ C#/C# 1 Contests/Problem 1/Volleyball/Volleyball-AS/Voleyball.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Volleyball
+{
+    static void Main()
+    {
+        string leap = Console.ReadLine();
+        int hollidaysCount = int.Parse(Console.ReadLine());
+        int weekendsHomeCount = int.Parse(Console.ReadLine());
+
+        int weekendsInYear = 48;
+        int normalWeekendsCount = weekendsInYear - weekendsHomeCount;
+        double gamesCount =
+            normalWeekendsCount * 3.0 / 4.0 +
+            weekendsHomeCount * 1.0 +
+            hollidaysCount * 2.0 / 3.0;
+        if (leap == "leap")
+        {
+            gamesCount = gamesCount * 1.15;
+        }
+        Console.WriteLine((int)gamesCount);
+    }
+}
