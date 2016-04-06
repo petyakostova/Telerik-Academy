897d102c1d7a08377b693a5179b1d6c70fa3fadf
petyakostova <petya_kostova_@abv.bg>
2016-04-06 11:39:41 +0300

    add Electricity

---
+++ C#/C# 1 Contests/Problem 1/Electricity/1. Electricity/Electricity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _1.Electricity
+{
+    class Electricity
+    {
+        static void Main()
+        {
+            double lampPower = 100.53;
+            double compPower = 125.90;
+
+            int numberOfFloors = int.Parse(Console.ReadLine());
+            int flatsPerFloor = int.Parse(Console.ReadLine());
+
+            DateTime time = DateTime.Parse(Console.ReadLine());
+
+            DateTime firstStart = DateTime.Parse("00:00");
+            DateTime firstEnd = DateTime.Parse("08:59");
+            DateTime secondStart = DateTime.Parse("14:00");
+            DateTime secondEnd = DateTime.Parse("18:59");
+            DateTime thirdStart = DateTime.Parse("19:00");
+            DateTime thirdEnd = DateTime.Parse("23:59");
+
+            // power consumption for one flat
+            double totalPower = 0;
+
+            if (firstStart <= time && time <= firstEnd)
+            {
+                totalPower += lampPower + (compPower * 8);
+            }
+            else if (secondStart <= time && time <= secondEnd)
+            {
+                totalPower += (lampPower * 2) + (compPower * 2);
+            }
+            else if (thirdStart <= time && time <= thirdEnd)
+            {
+                totalPower += (lampPower * 7) + (compPower * 6);
+            }
+
+            totalPower *= numberOfFloors * flatsPerFloor;
+
+            Console.WriteLine("{0} Watts", Math.Floor(totalPower));
+            //Console.WriteLine("{0} Watts", (int)(totalPower));
+        }
+    }
+}
---
+++ C#/C# 1 Contests/Problem 1/Electricity/Electricity-AS/Electricity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Electricity
+{
+    public static void Main()
+    {
+        int floors = int.Parse(Console.ReadLine());
+        int flats = int.Parse(Console.ReadLine());
+        DateTime time = DateTime.Parse(Console.ReadLine());
+
+        int sumWats;
+        if (time.Hour >= 14 && time.Hour <= 18)
+        {
+            // Noon time
+            double noonConsumption = (2 * 100.53) + (2 * 125.90);
+            sumWats = (int)(noonConsumption * flats * floors);
+        }
+        else if (time.Hour >= 19 && time.Hour <= 23)
+        {
+            // Afternoon time
+            double afternoonConsumption = (7 * 100.53) + (6 * 125.90);
+            sumWats = (int)(afternoonConsumption * flats * floors);
+        }
+        else if (time.Hour >= 0 && time.Hour <= 8)
+        {
+            // Midnight time
+            double midnightConsumption = (1 * 100.53) + (8 * 125.90);
+            sumWats = (int)(midnightConsumption * flats * floors);
+        }
+        else
+        {
+            // Any other time --> zero consumption
+            sumWats = 0;
+        }
+
+        Console.WriteLine(sumWats + " Watts");
+    }
+}
