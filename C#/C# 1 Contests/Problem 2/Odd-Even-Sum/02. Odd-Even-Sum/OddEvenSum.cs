e4894f0071e04f60f0b6e548b98b4718ec58c216
petyakostova <petya_kostova_@abv.bg>
2016-04-07 03:20:56 +0300

    add Odd-Even-Sum

---
+++ C#/C# 1 Contests/Problem 2/Odd-Even-Sum/02. Odd-Even-Sum/OddEvenSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class OddEvenSum
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        long oddSum = 0;
+        long evenSum = 0;
+
+        for (int i = 1; i <= 2 * n; i++)
+        {
+            int currentNum = int.Parse(Console.ReadLine());
+            if (i % 2 != 0)
+            {
+                oddSum += currentNum;
+            }
+            else
+            {
+                evenSum += currentNum;
+            }
+        }
+
+        if (oddSum == evenSum)
+        {
+            Console.WriteLine("Yes, sum={0}", oddSum);
+        }
+        else
+        {
+            Console.WriteLine("No, diff={0}", Math.Abs(oddSum - evenSum));
+        }
+    }
+}
---
+++ C#/C# 1 Contests/Problem 2/Odd-Even-Sum/Odd-Even-Sum-AS/OddEvenSumAS.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class OddEvenSumAS
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sumOdd = 0;
+        int sumEven = 0;
+        bool odd = true;
+
+        for (int i = 0; i < 2 * n; i++)
+        {
+            int element = int.Parse(Console.ReadLine());
+            if (odd)
+            {
+                sumOdd = sumOdd + element;
+            }
+            else
+            {
+                sumEven = sumEven + element;
+            }
+            odd = !odd;
+        }
+
+        if (sumOdd == sumEven)
+        {
+            Console.WriteLine("Yes, sum=" + sumOdd);
+        }
+        else
+        {
+            int diff = Math.Abs(sumOdd - sumEven);
+            Console.WriteLine("No, diff=" + diff);
+        }
+    }
+}
