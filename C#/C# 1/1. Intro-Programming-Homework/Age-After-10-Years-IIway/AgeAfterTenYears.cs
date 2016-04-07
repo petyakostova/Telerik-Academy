dec364d72cc1cbc7f069944a55f7691a771495e3
petyakostova <petya_kostova_@abv.bg>
2016-04-07 12:40:01 +0300

    add Age-After-10-Years-IIway

---
+++ C#/C# 1/1. Intro-Programming-Homework/Age-After-10-Years-IIway/AgeAfterTenYears.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class AgeAfterTenYears
+{
+    static void Main()
+    {
+        DateTime birthDate = DateTime.ParseExact(Console.ReadLine(), "MM.dd.yyyy", null);
+
+        int Days = (DateTime.Now.Year * 365 + DateTime.Now.DayOfYear)
+            - (birthDate.Year * 365 + birthDate.DayOfYear);
+        int Years = Days / 365;
+
+        Console.WriteLine(Years);
+        Console.WriteLine(Years + 10);
+    }
+}
