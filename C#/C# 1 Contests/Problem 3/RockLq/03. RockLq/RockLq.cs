f09f4ad6ce7e409c3289b419d0da3bd3ad687fb8
petyakostova <petya_kostova_@abv.bg>
2016-04-09 02:59:17 +0300

    rename folders with tests and create 03. RockLq Solution

---
+++ C#/C# 1 Contests/Problem 3/RockLq/03. RockLq/RockLq.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class RockLq
+{
+    static void Main()
+    {
+
+    }
+}
---
+++ C#/C# 1 Contests/Problem 3/RockLq/RockLq-AS/RockLqAS.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class RockLqAS
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("{0}{1}{0}", new string('.', n), new string('*', n));
+
+        int dotsBefore = n;
+        int dotsMiddle = n + 2;
+        for (int i = 0; i < n / 2; i++)
+        {
+            dotsBefore -= 2;
+            Console.WriteLine("{0}*{1}*{0}", new string('.', dotsBefore), new string('.', dotsMiddle));
+            dotsMiddle += 4;
+        }
+
+        dotsBefore = n - 2;
+        dotsMiddle = n;
+        int dotsBetween = 1;
+        Console.WriteLine("*{0}*{1}*{0}*", new string('.', dotsBefore), new string('.', dotsMiddle));
+        for (int i = 0; i < (n / 2) - 1; i++)
+        {
+            dotsBefore -= 2;
+            Console.WriteLine("*{0}*{2}*{1}*{2}*{0}*",
+                new string('.', dotsBefore), new string('.', dotsMiddle), new string('.', dotsBetween));
+            dotsBetween += 2;
+        }
+
+        dotsBefore = n - 2;
+        dotsBefore++;
+        for (int i = 0; i < n - 1; i++)
+        {
+            Console.WriteLine("{0}*{1}*{0}", new string('.', dotsBefore), new string('.', dotsMiddle));
+            dotsBefore--;
+            dotsMiddle += 2;
+        }
+
+        Console.WriteLine(new string('*', n * 3));
+    }
+}
