7e5a0dd3fec34372fc2651ab1dafeeecd9f09242
petyakostova <petya_kostova_@abv.bg>
2016-04-09 02:05:03 +0300

    add 01. Exam Schedule

---
+++ C#/C# 1 Contests/Problem 1/Exam-Schedule/01. Exam-Schedule/ExamSchedule.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class ExamSchedule
+{
+    static void Main()
+    {
+        int startingHour = int.Parse(Console.ReadLine());
+        int startingMinutes = int.Parse(Console.ReadLine());
+        string startingTimeOfDay = Console.ReadLine();
+        int durationHours = int.Parse(Console.ReadLine());
+        int durationMinutes = int.Parse(Console.ReadLine());
+
+        int endingHour = startingHour + durationHours;
+        int endingMinutes = startingMinutes + durationMinutes;
+        string endingTimeOfDay = startingTimeOfDay;
+
+        if (endingMinutes >= 60)
+        {
+            endingHour++;
+            endingMinutes -= 60;
+        }
+
+        if (endingHour >= 12 && endingHour < 24)
+        {
+            if (endingHour != 12)
+            {
+                endingHour -= 12;
+            }
+            if (startingTimeOfDay == "AM")
+            {
+                endingTimeOfDay = "PM";
+            }
+            else
+            {
+                endingTimeOfDay = "AM";
+            }
+        }
+        else if (endingHour >= 24)
+        {
+            endingHour -= 24;
+        }
+
+        Console.WriteLine("{0:00}:{1:00}:{2}", endingHour, endingMinutes, endingTimeOfDay);
+    }
+}
---
+++ C#/C# 1 Contests/Problem 1/Exam-Schedule/Exam-Schedule-AS/ExamScheduleAS.cs
@@ -0,0 +1,28 @@
+﻿// Author's Solution
+using System;
+
+class ExamScheduleAS
+{
+    static void Main()
+    {
+        int startHour = int.Parse(Console.ReadLine());
+        int startMinutes = int.Parse(Console.ReadLine());
+        string partOfDay = Console.ReadLine();
+        int durationHours = int.Parse(Console.ReadLine());
+        int durationMinutes = int.Parse(Console.ReadLine());
+
+        if (partOfDay == "PM")
+        {
+            startHour += 12;
+
+            if (startHour == 24)
+            {
+                startHour = 0;
+            }
+        }
+
+        DateTime startTime = new DateTime(2014, 04, 09, startHour, startMinutes, 0);
+        DateTime endTime = startTime.AddHours(durationHours).AddMinutes(durationMinutes);
+        Console.WriteLine("{0:hh:mm:tt}", endTime);
+    }
+}
