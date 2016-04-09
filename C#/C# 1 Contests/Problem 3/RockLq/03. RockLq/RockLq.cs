9b28e8588837ad05cc3cdcfb512635082b39613a
petyakostova <petya_kostova_@abv.bg>
2016-04-09 03:20:03 +0300

    solve RockLq and put some comments in RockLq-AS

--- C#/C# 1 Contests/Problem 3/RockLq/03. RockLq/RockLq.cs
+++ C#/C# 1 Contests/Problem 3/RockLq/03. RockLq/RockLq.cs
@@ -4,6 +4,44 @@ class RockLq
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
 
+        string dotsOut = new string('.', n);
+        string asterix = new string('*', n);
+        string dotsIn = "";
+        Console.WriteLine("{0}{1}{0}", dotsOut, asterix);   // part 1
+        //Console.WriteLine(dotsOut + asterix + dotsOut);         
+
+        for (int i = 0; i < n / 2; i++)
+        {
+            dotsOut = new string('.', n - 2 - 2 * i);
+            dotsIn = new string('.', n + 2 + 4 * i);
+            Console.WriteLine("{0}*{1}*{0}", dotsOut, dotsIn);   // part 2
+            //Console.WriteLine(dotsOut + "*" + dotsIn + "*" + dotsOut);
+        }
+
+        dotsOut = new string('.', n - 2);
+        dotsIn = new string('.', n);
+        Console.WriteLine("*{0}*{1}*{0}*", dotsOut, dotsIn);   // part 3
+        //Console.WriteLine("*" + dotsOut + "*" + dotsIn + "*" + dotsOut + "*");
+        
+        for (int i = 0; i < n / 2 - 1; i++)
+        {
+            string dotsR = new string('.', n - 4 - 2 * i);
+            dotsOut = new string('.', 1 + 2 * i);
+            Console.WriteLine("*{0}*{1}*{2}*{1}*{0}*", dotsR, dotsOut, dotsIn);   // part 4
+            //Console.WriteLine("*" + dotsR + "*" + dotsOut + "*" + dotsIn + "*" + dotsOut + "*" + dotsR + "*");
+        }
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            dotsOut = new string('.', n - 1 - i);
+            dotsIn = new string('.', n + 2 * i);
+            Console.WriteLine("{0}*{1}*{0}", dotsOut, dotsIn);   // part 5
+            //Console.WriteLine(dotsOut + "*" + dotsIn + "*" + dotsOut);
+        }
+
+        asterix = new string('*', 3 * n);
+        Console.WriteLine(asterix);   // part 6
     }
 }
--- C#/C# 1 Contests/Problem 3/RockLq/RockLq-AS/RockLqAS.cs
+++ C#/C# 1 Contests/Problem 3/RockLq/RockLq-AS/RockLqAS.cs
@@ -6,26 +6,27 @@ class RockLqAS
     {
         int n = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("{0}{1}{0}", new string('.', n), new string('*', n));
+        Console.WriteLine("{0}{1}{0}", new string('.', n), new string('*', n)); // part 1
 
         int dotsBefore = n;
         int dotsMiddle = n + 2;
         for (int i = 0; i < n / 2; i++)
         {
             dotsBefore -= 2;
-            Console.WriteLine("{0}*{1}*{0}", new string('.', dotsBefore), new string('.', dotsMiddle));
+            Console.WriteLine("{0}*{1}*{0}", new string('.', dotsBefore), new string('.', dotsMiddle)); // part 2
             dotsMiddle += 4;
         }
 
         dotsBefore = n - 2;
         dotsMiddle = n;
         int dotsBetween = 1;
-        Console.WriteLine("*{0}*{1}*{0}*", new string('.', dotsBefore), new string('.', dotsMiddle));
+        Console.WriteLine("*{0}*{1}*{0}*", new string('.', dotsBefore), new string('.', dotsMiddle)); // part 3
+
         for (int i = 0; i < (n / 2) - 1; i++)
         {
             dotsBefore -= 2;
             Console.WriteLine("*{0}*{2}*{1}*{2}*{0}*",
-                new string('.', dotsBefore), new string('.', dotsMiddle), new string('.', dotsBetween));
+                new string('.', dotsBefore), new string('.', dotsMiddle), new string('.', dotsBetween)); // part 4
             dotsBetween += 2;
         }
 
@@ -33,11 +34,11 @@ class RockLqAS
         dotsBefore++;
         for (int i = 0; i < n - 1; i++)
         {
-            Console.WriteLine("{0}*{1}*{0}", new string('.', dotsBefore), new string('.', dotsMiddle));
+            Console.WriteLine("{0}*{1}*{0}", new string('.', dotsBefore), new string('.', dotsMiddle)); // part 5
             dotsBefore--;
             dotsMiddle += 2;
         }
 
-        Console.WriteLine(new string('*', n * 3));
+        Console.WriteLine(new string('*', n * 3)); // part 6
     }
 }
