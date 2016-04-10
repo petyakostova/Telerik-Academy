9433f750e4cd419a75d5680c0e112020ee5a49ed
petyakostova <petya_kostova_@abv.bg>
2016-04-11 02:39:53 +0300

    add House-With-A-Window-AS/HouseWithWindowAS.cs and 03. House-With-A-Window/HouseWithAWindow.cs

---
+++ C#/C# 1 Contests/Problem 3/House-With-A-Window/03. House-With-A-Window/HouseWithAWindow.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class HouseWithAWindow
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int houseWidth = 2 * n - 1;
+
+        PrintRoof(n); // part 1 & 2
+
+        Console.WriteLine("{0}", new string('*', houseWidth)); // part 3
+
+        PrintHouseBase(n, houseWidth); // part 4
+
+        for (int i = 0; i < n / 2; i++)
+        {
+            Console.WriteLine("*{0}{1}{0}*",
+                new string('.', n / 2),
+                new string('*', n - 3)); // part 5
+        }
+
+        PrintHouseBase(n, houseWidth); // part 6
+
+        Console.WriteLine("{0}", new string('*', houseWidth)); // part 7
+    }
+
+    static void PrintHouseBase(int n, int houseWidth) 
+    {
+        for (int i = 0; i < n / 4; i++)
+        {
+            Console.WriteLine("*{0}*", new string('.', houseWidth - 2));  // part 4;6
+        }
+    }
+
+    static void PrintRoof(int n)
+    {
+        Console.WriteLine("{0}*{0}", new string('.', n - 1)); // part 1
+
+        int outerPoints = n - 2;
+        int innerPoints = 1;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            Console.WriteLine("{0}*{1}*{0}",
+                new string('.', outerPoints),
+                new string('.', innerPoints)); // part 2
+            outerPoints--;
+            innerPoints += 2;
+        }
+    }
+
+}
---
+++ C#/C# 1 Contests/Problem 3/House-With-A-Window/House-With-A-Window-AS/HouseWithWindowAS.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class HouseWithWindowAS
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int starsBefore = n - 1;
+        int starsMiddle = 1;
+
+        Console.WriteLine("{0}*{0}", new string('.', starsBefore)); // part 1
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            starsBefore--;
+            Console.WriteLine("{0}*{1}*{0}", new string('.', starsBefore), new string('.', starsMiddle)); // part 2
+            starsMiddle += 2;
+        }
+
+        Console.WriteLine(new string('*', 2 * n - 1)); // part 3
+
+        for (int i = 0; i < n / 4; i++)
+        {
+            Console.WriteLine("*{0}*", new string('.', n * 2 - 3)); // part 4
+        }
+
+        int dotsBetweenWindows = n / 2;
+        int windowsStars = n - 3;
+        for (int i = 0; i < n / 2; i++)
+        {
+            Console.WriteLine("*{0}{1}{0}*",
+                new string('.', dotsBetweenWindows), new string('*', windowsStars)); // part 5
+        }
+
+        for (int i = 0; i < n / 4; i++)
+        {
+            Console.WriteLine("*{0}*", new string('.', n * 2 - 3)); // part 6
+        }
+
+        Console.WriteLine(new string('*', 2 * n - 1)); // part 7
+    }
+}
