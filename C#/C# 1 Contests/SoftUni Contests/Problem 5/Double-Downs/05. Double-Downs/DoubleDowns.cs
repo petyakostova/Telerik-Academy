18984bc2d6609eed2f282285ed78413370466f0c
petyakostova <petya_kostova_@abv.bg>
2016-04-12 01:24:39 +0300

    add Double-Downs/05. Double-Downs/DoubleDowns.cs and Double-Downs-AS/DoubleDownsAS.cs

---
+++ C#/C# 1 Contests/SoftUni Contests/Problem 5/Double-Downs/05. Double-Downs/DoubleDowns.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class DoubleDowns
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) //n = numbers.Length
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        int rightDiagonal = 0;
+        int leftDiagonal = 0;
+        int vertical = 0;
+
+        for (int i = 0; i < n - 1; i++) //n = numbers.Length
+        {
+            int upNum = numbers[i];
+            int downNum = numbers[i + 1];
+
+            for (int bit = 0; bit < 32; bit++) //because we use int (32-bit numbers)
+            {
+                int mask = 1 << bit;
+
+                // check if the current bit is 1
+                bool upperNumberMatch = (upNum & mask) > 0;
+
+                if (upperNumberMatch && (downNum & mask) > 0)
+                {
+                    vertical++;
+                }
+                // shift the mask to the left to catch the left diagonal
+                if (upperNumberMatch && (downNum & mask << 1) > 0)
+                {
+                    leftDiagonal++;
+                }
+                // shift the mask to the right to catch the left diagonal
+                if (upperNumberMatch && (downNum & mask >> 1) > 0)
+                {
+                    rightDiagonal++;
+                }
+            }
+        }
+
+        Console.WriteLine(rightDiagonal);
+        Console.WriteLine(leftDiagonal);
+        Console.WriteLine(vertical);
+    }
+}
---
+++ C#/C# 1 Contests/SoftUni Contests/Problem 5/Double-Downs/Double-Downs-AS/DoubleDownsAS.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class DoubleDownsAS
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int[] numbers = new int[n];
+
+        int leftDiagonalCounter = 0;
+        int rightDiagonalCounter = 0;
+        int downCounter = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int bit = 0; bit < 32; bit++)
+            {
+                int upNumber = numbers[i];
+                int downNumber = numbers[i + 1];
+
+                int mask = 1 << bit;
+                bool checkUp = (upNumber & mask) > 0;
+
+                if (checkUp && (downNumber & (mask << 1)) > 0)
+                {
+                    leftDiagonalCounter++;
+                }
+                if (checkUp && (downNumber & mask) > 0)
+                {
+                    downCounter++;
+                }
+                if (bit > 0 && checkUp && (downNumber & (mask >> 1)) > 0)
+                {
+                    rightDiagonalCounter++;
+                }
+            }
+        }
+
+        Console.WriteLine(rightDiagonalCounter);
+        Console.WriteLine(leftDiagonalCounter);
+        Console.WriteLine(downCounter);
+    }
+}
