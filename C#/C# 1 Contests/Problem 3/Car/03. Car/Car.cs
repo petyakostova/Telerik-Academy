a3e11631c3db429b881b40f7970d81360c385b96
petyakostova <petya_kostova_@abv.bg>
2016-04-11 03:30:38 +0300

    add Car-AS/CarsAS.cs and 03. Car/Car.cs; car example 1 & 2.jpg; car example 3.jpg

---
+++ C#/C# 1 Contests/Problem 3/Car/03. Car/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Car
+{
+    static void Main()
+    {
+
+        int n = int.Parse(Console.ReadLine());
+
+        // Car roof
+        string dots = new string('.', n);
+        string asterix = new string('*', n);
+        Console.WriteLine(dots + asterix + dots); // part 1
+
+        for (int i = 0; i < n / 2 - 1; i++)
+        {
+            string dotsOut = new string('.', n - 1 - i);
+            string dotsIn = new string('.', n + 2 * i);
+            Console.WriteLine(dotsOut + "*" + dotsIn + "*" + dotsOut); // part 2
+        }
+
+        //Car body
+        asterix = new string('*', n / 2 + 1);
+        dots = new string('.', 2 * n - 2); // 3*n - 2*(n / 2 + 1) = 3*n - n - 2 = 2*n - 2
+        Console.WriteLine(asterix + dots + asterix); // part 3
+
+        for (int i = 0; i < n / 2 - 2; i++)
+        {
+            dots = new string('.', 3 * n - 2);
+            Console.WriteLine("*" + dots + "*"); // part 4
+        }
+
+        asterix = new string('*', 3 * n);
+        Console.WriteLine(asterix); // part 5
+
+        //Car tyres
+        for (int i = 0; i < n / 2 - 2; i++)
+        {
+            dots = new string('.', n / 2);
+            string dotsIn = new string('.', n / 2 - 1);
+            Console.WriteLine(dots + "*" + dotsIn + "*" + dotsIn + dotsIn + "*" + dotsIn + "*" + dots); // part 6
+        }
+
+        asterix = new string('*', n / 2 + 1);
+        string dotsBetween = new string('.', n - 2); // 2*(n/2 - 1) = n-2
+        Console.WriteLine(dots + asterix + dotsBetween + asterix + dots); // part 7
+    }
+}
---
+++ C#/C# 1 Contests/Problem 3/Car/Car-AS/CarsAS.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class CarsAS
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int dots = n;
+        int stars = n;
+        int dotsMiddle = stars;
+
+        Console.WriteLine("{0}{1}{0}", new string('.', dots), new string('*', stars)); // part 1
+
+        for (int i = 0; i < n / 2 - 1; i++)
+        {
+            dots--;
+            Console.WriteLine("{0}*{1}*{0}", new string('.', dots), new string('.', dotsMiddle)); // part 2
+            dotsMiddle += 2;
+        }
+
+        Console.WriteLine("{0}{1}{0}", new string('*', dots), new string('.', n * 2 - 2)); // part 3  dots=n/2 +1
+
+        for (int i = 0; i < n / 2 - 2; i++)
+        {
+            Console.WriteLine("*{0}*", new string('.', n * 3 - 2)); // part 4
+        }
+
+        int middleDots = (n / 2 + 1) + (n - 6) / 2; // n - 2
+
+        Console.WriteLine(new string('*', n * 3)); // part 5
+
+        for (int i = 0; i < n / 2 - 2; i++)
+        {
+            Console.WriteLine("{0}*{1}*{2}*{1}*{0}",
+                new string('.', n / 2), new string('.', n / 2 - 1), new string('.', middleDots)); // part 6
+        }
+
+        Console.WriteLine("{0}{1}{2}{1}{0}",
+            new string('.', n / 2), new string('*', n / 2 + 1), new string('.', middleDots)); // part 7
+    }
+}
