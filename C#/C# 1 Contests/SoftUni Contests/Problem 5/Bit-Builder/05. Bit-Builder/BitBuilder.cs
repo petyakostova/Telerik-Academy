9fd326ff3f6bd17ef4f5f8258acfe84807320cac
petyakostova <petya_kostova_@abv.bg>
2016-04-12 02:19:26 +0300

    add Bit-Builder-AS/BitBuilderAS.cs and 05. Bit-Builder/BitBuilder.cs => 50/100

---
+++ C#/C# 1 Contests/SoftUni Contests/Problem 5/Bit-Builder/05. Bit-Builder/BitBuilder.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+class BitBuilder
+{
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "quit")
+            {
+                break;
+            }
+
+            int position = int.Parse(input);
+
+            string order = Console.ReadLine();
+
+            switch (order)
+            {
+                case "flip":
+                    num = FlipBit(num, position);
+                    break;
+                case "insert":
+                    num = InsertBit(num, position);
+                    break;
+                case "remove":
+                    num = RemoveBit(num, position);
+                    break;
+            }
+        }
+
+        Console.WriteLine(num);
+    }
+
+    static int RemoveBit(int number, int pos)
+    {
+        int result = 0;
+        int addition = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            int mask = 1 << i;
+            if (i == pos)
+            {
+                addition = 1;
+                continue;
+            }
+            result += (number & mask) >> addition;
+        }
+        return result;
+    }
+
+    static int InsertBit(int number, int pos)
+    {
+        int result = 0;
+        int addition = 0;
+        for (int i = 0; i < 32; i++)
+        {
+
+            int mask = 1 << i;
+            if (i == pos)
+            {
+                result += 1 << pos;
+                addition = 1;
+            }
+            result += (number & mask) << addition;
+        }
+        return result;
+    }
+
+    static int FlipBit(int num, int pos)
+    {
+        int mask = 1 << pos;
+
+        int bit = mask & num;
+
+        if (bit == 1)
+        {
+            num = num & ~mask;
+        }
+        else
+        {
+            num = num | mask;
+        }
+
+        return num;
+    }
+
+}
---
+++ C#/C# 1 Contests/SoftUni Contests/Problem 5/Bit-Builder/Bit-Builder-AS/BitBuilderAS.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+class BitBuilderAS
+{
+    static int bitCount = 0;
+
+    static void Main()
+    {
+        long number = long.Parse(Console.ReadLine());
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            int position;
+
+            countBits(number);
+
+            if (input != "quit")
+            {
+                position = int.Parse(input);
+            }
+            else
+            {
+                break;
+            }
+
+            string command = Console.ReadLine();
+
+            switch (command)
+            {
+                case "flip":
+                    number ^= (long)1 << position;
+                    break;
+                case "remove":
+                    number = removeBit(position, number);
+                    break;
+                case "insert":
+                    number = insertBit(position, number);
+                    break;
+            }
+        }
+
+        Console.WriteLine(number);
+    }
+
+    private static long removeBit(int position, long number)
+    {
+        long currentNumber = 0;
+        bool changeOccured = false;
+
+        for (int bit = 0; bit < bitCount; bit++)
+        {
+            long currentBit = (number >> bit) & 1;
+
+            if (bit != position)
+            {
+                currentNumber >>= 1;
+                currentNumber |= currentBit << (bitCount - 1);
+            }
+            else
+            {
+                changeOccured = true;
+            }
+        }
+
+        if (changeOccured)
+        {
+            currentNumber >>= 1;
+        }
+        return currentNumber;
+    }
+
+    private static long insertBit(int position, long number)
+    {
+        long currentNum = 0;
+        bool changeOccured = false;
+
+        for (int bit = 0; bit < bitCount; bit++)
+        {
+            currentNum >>= 1;
+
+            if (bit == position)
+            {
+                currentNum |= (long)1 << bitCount;
+                bit--;
+                position = -1;
+                changeOccured = true;
+            }
+            else
+            {
+                long currentBit = (number >> bit) & 1;
+                currentNum |= currentBit << bitCount;
+            }
+        }
+
+        if (!changeOccured)
+        {
+            currentNum >>= 1;
+            currentNum |= (long)1 << position;
+        }
+
+        return currentNum;
+    }
+
+    private static void countBits(long number)
+    {
+        long numToCount = number;
+        bitCount = 0;
+
+        while (numToCount > 0)
+        {
+            bitCount++;
+            numToCount >>= 1;
+        }
+    }
+}
