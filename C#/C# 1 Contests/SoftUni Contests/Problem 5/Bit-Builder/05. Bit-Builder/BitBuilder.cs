ec42af19589d54b7af5025bd70db1a36c451f630
petyakostova <petya_kostova_@abv.bg>
2016-04-12 03:04:51 +0300

    modify Bit-Builder/05. Bit-Builder/BitBuilder.cs => 70/100

--- C#/C# 1 Contests/SoftUni Contests/Problem 5/Bit-Builder/05. Bit-Builder/BitBuilder.cs
+++ C#/C# 1 Contests/SoftUni Contests/Problem 5/Bit-Builder/05. Bit-Builder/BitBuilder.cs
@@ -39,16 +39,21 @@ class BitBuilder
     {
         int result = 0;
         int addition = 0;
+
         for (int i = 0; i < 32; i++)
         {
             int mask = 1 << i;
+
             if (i == pos)
             {
                 addition = 1;
                 continue;
             }
+
             result += (number & mask) >> addition;
         }
+
+        //Console.WriteLine(Convert.ToString(result, 2).PadLeft(16, '0')); //medium check
         return result;
     }
 
@@ -56,35 +61,42 @@ class BitBuilder
     {
         int result = 0;
         int addition = 0;
+
         for (int i = 0; i < 32; i++)
         {
-
             int mask = 1 << i;
+
             if (i == pos)
             {
                 result += 1 << pos;
                 addition = 1;
             }
+
             result += (number & mask) << addition;
         }
+
+        //Console.WriteLine(Convert.ToString(result, 2).PadLeft(16, '0')); //medium check
         return result;
     }
 
     static int FlipBit(int num, int pos)
     {
+        // get the bit at position pos from a number num
+        int bit = (num >> pos) & 1;
+
         int mask = 1 << pos;
-
-        int bit = mask & num;
-
         if (bit == 1)
         {
+            // set the bit at position pos to 0
             num = num & ~mask;
         }
         else
         {
+            // set the bit at position pos to 1
             num = num | mask;
         }
 
+        //Console.WriteLine(Convert.ToString(num, 2).PadLeft(16, '0')); //medium check
         return num;
     }
 
