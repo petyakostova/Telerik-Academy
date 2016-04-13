91934efc0cd519491f91c0a6fd3d907dd43fb717
petyakostova <petya_kostova_@abv.bg>
2016-04-13 08:29:59 +0300

    add 3. Headphones/Headphones2.cs => second way

---
+++ C#/C# 1 Contests/SoftUni Contests/3/Headphones/3. Headphones/Headphones2.cs
@@ -0,0 +1,42 @@
+﻿// second way
+
+using System;
+
+class Headphones2
+{
+    static void Main()
+    {
+        int d = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("{0}{1}{0}", new string('-', d / 2), new string('*', d + 2)); // part 1
+
+        for (int i = 0; i < d - 1; i++)
+        {
+            Console.WriteLine("{0}*{1}*{0}", new string('-', d / 2), new string('-', d)); // part 2
+        }
+
+        int dashesOut = d / 2;
+        int stars = 1;
+        int dashesIn = d;
+        for (int i = 0; i < d / 2; i++)
+        {
+            Console.WriteLine("{0}{1}{2}{1}{0}", new string('-', dashesOut), new string('*', stars), new string('-', dashesIn)); // part 3
+            stars += 2;
+            dashesOut--;
+            dashesIn -= 2;
+        }
+
+        Console.WriteLine("{0}-{0}", new string('*', d)); // part 4
+
+        dashesOut = 1;
+        stars = d - 2;
+        dashesIn = 3;
+        for (int i = 0; i < d / 2; i++)
+        {
+            Console.WriteLine("{0}{1}{2}{1}{0}", new string('-', dashesOut), new string('*', stars), new string('-', dashesIn)); // part 5
+            stars -= 2;
+            dashesOut++;
+            dashesIn += 2;
+        }
+    }
+}
