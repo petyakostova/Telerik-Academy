6aa937ef8985e753e0059b52c8718d9eb918ced5
petyakostova <petya_kostova_@abv.bg>
2016-04-15 05:18:41 +0300

    solve Coffee-Machine/01. Coffee-Machine/CoffeeMachine.cs

--- C#/C# 1 Contests/1/Coffee-Machine/01. Coffee-Machine/CoffeeMachine.cs
+++ C#/C# 1 Contests/1/Coffee-Machine/01. Coffee-Machine/CoffeeMachine.cs
@@ -1,9 +1,40 @@
 ﻿using System;
+using System.Globalization;
+using System.Threading;
 
 class CoffeeMachine
 {
     static void Main()
     {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+        int n3 = int.Parse(Console.ReadLine());
+        int n4 = int.Parse(Console.ReadLine());
+        int n5 = int.Parse(Console.ReadLine());
+        decimal amount = decimal.Parse(Console.ReadLine());
+        decimal price = decimal.Parse(Console.ReadLine());
+
+        if (price > amount)
+        {
+            Console.WriteLine("More {0:F2}", price - amount);
+        }
+        else
+        {
+            decimal moneyInMashine = 0.05m * n1 + 0.10m * n2 + 0.20m * n3 + 0.50m * n4 + 1.00m * n5;
+            decimal change = amount - price;
+
+            if (change > moneyInMashine)
+            {
+                Console.WriteLine("No {0:F2}", change - moneyInMashine);
+            }
+            else
+            {
+                Console.WriteLine("Yes {0:F2}", moneyInMashine - change);
+            }
+
+        }
 
     }
 }
