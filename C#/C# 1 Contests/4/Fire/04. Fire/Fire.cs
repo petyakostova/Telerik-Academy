c88620252e5b510def022b67118ee710cd809961
petyakostova <petya_kostova_@abv.bg>
2016-04-15 04:47:28 +0300

    add new solutions to Fire.sln

--- C#/C# 1 Contests/4/Fire/04. Fire/Fire.cs
+++ C#/C# 1 Contests/4/Fire/04. Fire/Fire.cs
@@ -13,25 +13,28 @@ class Fire
             Console.WriteLine("{0}#{1}#{0}", new string('.', (n / 2 - 2) - i), new string('.', 2 + 2 * i)); // part 2
         }
 
-        for (int i = 0; i < 2; i++)
+        Console.WriteLine("#{0}#", new string('.', n - 2)); // part 3
+
+        for (int i = 0; i < n / 4; i++)
         {
-            Console.WriteLine("#{0}#", new string('.', n - 2)); // part 3
+            Console.WriteLine("{0}#{1}#{0}", new string('.', i), new string('.', n - 2 - 2 * i)); // part 4
         }
 
-        int outDots = 1;
-        int middleDots = n - 4;
-        while (middleDots >= n / 2)
-        {
-            Console.WriteLine("{0}#{1}#{0}", new string('.', outDots), new string('.', middleDots)); // part 4
-            outDots++;
-            middleDots -= 2;
-        }
+        // other way for part 4
+        //int outDots = 0;
+        //int middleDots = n - 2;
+        //while (middleDots >= n / 2)
+        //{
+        //    Console.WriteLine("{0}#{1}#{0}", new string('.', outDots), new string('.', middleDots)); // part 4
+        //    outDots++;
+        //    middleDots -= 2;
+        //}
 
         Console.WriteLine(new string('-', n)); // part 5
 
         for (int i = 0; i < n / 2; i++)
         {
-            Console.WriteLine("{0}{1}{2}{0}", 
+            Console.WriteLine("{0}{1}{2}{0}",
                 new string('.', i), new string('\\', n / 2 - i), new string('/', n / 2 - i)); // part 6
         }
 
