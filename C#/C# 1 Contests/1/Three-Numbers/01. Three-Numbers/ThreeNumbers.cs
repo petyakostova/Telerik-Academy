391ab2f893e0d2fdfc7cd6449d9210d13930031e
petyakostova <petya_kostova_@abv.bg>
2016-04-17 20:23:43 +0300

    solve Three-Numbers/01. Three-Numbers/ThreeNumbers.cs and update README.md

--- C#/C# 1 Contests/1/Three-Numbers/01. Three-Numbers/ThreeNumbers.cs
+++ C#/C# 1 Contests/1/Three-Numbers/01. Three-Numbers/ThreeNumbers.cs
@@ -1,11 +1,21 @@
-﻿using System;
-
-namespace _01.Three_Numbers
+﻿namespace _01.Three_Numbers
 {
+    using System;
+
     class ThreeNumbers
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Max(Math.Max(a, b), c));
+
+            Console.WriteLine(Math.Min(Math.Min(a, b), c));
+
+            double arithmeticMean = (a + b + c) / 3.0;
+            Console.WriteLine("{0:F2}", arithmeticMean);
         }
     }
 }
