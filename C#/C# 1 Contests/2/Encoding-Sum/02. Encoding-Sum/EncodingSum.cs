bcacc1f7a22de24bfb8a9f4557fe27c0fba08332
petyakostova <petya_kostova_@abv.bg>
2016-04-17 22:45:49 +0300

    solve 2/ Encoding-Sum and Text-To-Number and update README.md

--- C#/C# 1 Contests/2/Encoding-Sum/02. Encoding-Sum/EncodingSum.cs
+++ C#/C# 1 Contests/2/Encoding-Sum/02. Encoding-Sum/EncodingSum.cs
@@ -1,11 +1,35 @@
-﻿using System;
-
-namespace _02.Encoding_Sum
+﻿namespace _02.Encoding_Sum
 {
+    using System;
+
     class EncodingSum
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int module = int.Parse(Console.ReadLine());
+            string text = Console.ReadLine().ToUpper();
+            
+            int i = 0;
+            long result = 0;
+            while (text[i] != '@')
+            {
+                if (Char.IsDigit(text[i]))
+                {
+                    result *= text[i] - '0';
+                }
+                else if (Char.IsLetter(text[i]))
+                {
+                    result += text[i] - 'A';
+                }
+                else
+                {
+                    result %= module;
+                }
+
+                i++;
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
--- C#/C# 1 Contests/2/Encoding-Sum/Encoding-Sum-AS/EncodingSumAS.cs
+++ C#/C# 1 Contests/2/Encoding-Sum/Encoding-Sum-AS/EncodingSumAS.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TextToNumber.Solution
 {
