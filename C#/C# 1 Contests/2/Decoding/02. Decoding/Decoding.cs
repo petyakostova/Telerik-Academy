3c0924895d1a223105b504af152fb4c7c2d3114c
petyakostova <petya_kostova_@abv.bg>
2016-04-17 20:33:25 +0300

    solve Decoding/02. Decoding/Decoding.cs

--- C#/C# 1 Contests/2/Decoding/02. Decoding/Decoding.cs
+++ C#/C# 1 Contests/2/Decoding/02. Decoding/Decoding.cs
@@ -1,11 +1,43 @@
-﻿using System;
-
-namespace _02.Decoding
+﻿namespace _02.Decoding
 {
+    using System;
+
     class Decoding
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int saltNumber = int.Parse(Console.ReadLine());
+            string text = Console.ReadLine();
+
+            int i = 0;
+            while (text[i] != '@')
+            {
+                long encodedValue;
+                if (Char.IsLetter(text[i]))
+                {
+                    encodedValue = text[i] * saltNumber + 1000;
+                }
+                else if (Char.IsDigit(text[i]))
+                {
+                    encodedValue = text[i] + saltNumber + 500;
+                }
+                else
+                {
+                    encodedValue = text[i] - saltNumber;
+                }
+
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("{0:F2}", encodedValue / 100.0);
+                }
+                else
+                {
+                    Console.WriteLine(encodedValue * 100);
+                }
+
+                i++;
+            }
+
         }
     }
 }
