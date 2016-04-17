994e260c63a8d82e45b054d9ee053936352e15ec
petyakostova <petya_kostova_@abv.bg>
2016-04-17 21:43:41 +0300

    solve C# 1 Contests/1/ Money and Printing

--- C#/C# 1 Contests/1/Money/01. Money/Money.cs
+++ C#/C# 1 Contests/1/Money/01. Money/Money.cs
@@ -1,11 +1,25 @@
-﻿using System;
-
-namespace _01.Money
+﻿namespace _01.Money
 {
+    using System;
+    using System.Threading;
+    using System.Globalization;
+
     class Money
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            const double sheetsPerBox = 400;
+
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            
+            int studentsNumber = int.Parse(Console.ReadLine()); //N
+            int paperSheetsForStudent = int.Parse(Console.ReadLine()); //S
+            double priceBox = double.Parse(Console.ReadLine()); //P
+
+            double money = (studentsNumber * paperSheetsForStudent) / sheetsPerBox * priceBox;
+
+            Console.WriteLine("{0:F3}", money);
+            
         }
     }
 }
--- C#/C# 1 Contests/1/Printing/01. Printing/Printing.cs
+++ C#/C# 1 Contests/1/Printing/01. Printing/Printing.cs
@@ -1,11 +1,26 @@
-﻿using System;
-
-namespace _01.Printing
+﻿namespace _01.Printing
 {
+    using System;
+    using System.Globalization;
+    using System.Threading;
+
     class Printing
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
+            const double sheetsPerBox = 500;
+
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            int studentsNumber = int.Parse(Console.ReadLine()); //N
+            int paperSheetsForStudent = int.Parse(Console.ReadLine()); //S
+            double priceBox = double.Parse(Console.ReadLine()); //P
+
+            double money = (studentsNumber * paperSheetsForStudent) / sheetsPerBox * priceBox;
+
+            Console.WriteLine("{0:F2}", money);
+
         }
     }
 }
