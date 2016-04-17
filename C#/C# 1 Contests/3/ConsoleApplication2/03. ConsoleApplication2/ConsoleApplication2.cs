e4aa2b56a7697275146128fb8f631da9ec1145ce
petyakostova <petya_kostova_@abv.bg>
2016-04-17 19:18:37 +0300

    modify ConsoleApplication2.cs => 80/100 in bgcoder and add ConsoleApplication2/3. ConsoleApplication2/ConsoleApplicationString.cs => 80/100 in bgcoder

--- C#/C# 1 Contests/3/ConsoleApplication2/03. ConsoleApplication2/ConsoleApplication2.cs
+++ C#/C# 1 Contests/3/ConsoleApplication2/03. ConsoleApplication2/ConsoleApplication2.cs
@@ -1,4 +1,4 @@
-﻿// 70/100 in bgcoder
+﻿// 80/100 in bgcoder
 namespace _03.ConsoleApplication2
 {
     using System;
@@ -11,44 +11,40 @@ namespace _03.ConsoleApplication2
             string input = Console.ReadLine();
 
             int position = 0;
-            BigInteger product = 1;
+            BigInteger product10 = 1;
             BigInteger productAfter10 = 1;
 
             while (input != "END")
             {
-                if (position % 2 == 0)
+                if (position % 2 == 0 && input != "0")
                 {
                     BigInteger currentNumber = BigInteger.Parse(input);
 
-                    if (currentNumber != 0 && position < 10)
+                    BigInteger product = 1;
+                    while (currentNumber > 0)
                     {
-                        while (currentNumber > 0)
+                        if (currentNumber % 10 != 0)
                         {
-                            if (currentNumber % 10 != 0)
-                            {
-                                product *= currentNumber % 10;
-                            }                            
-                            currentNumber /= 10;
+                            product *= currentNumber % 10;
                         }
+                        currentNumber /= 10;
                     }
-                    else if (currentNumber != 0 && position >= 10)
+
+                    if (position < 10)
                     {
-                        while (currentNumber > 0)
-                        {
-                            if (currentNumber % 10 != 0)
-                            {
-                                productAfter10 *= currentNumber % 10;
-                            }
-                            currentNumber /= 10;
-                        }
+                        product10 *= product;
+                    }
+                    else //if (position >= 10)
+                    {
+                        productAfter10 *= product;
                     }
                 }
-                
+
                 input = Console.ReadLine();
                 position++;
             }
 
-            Console.WriteLine(product);
+            Console.WriteLine(product10);
             if (position > 9)
             {
                 Console.WriteLine(productAfter10);
---
+++ C#/C# 1 Contests/3/ConsoleApplication2/3. ConsoleApplication2/ConsoleApplicationString.cs
@@ -0,0 +1,49 @@
+﻿// 80/100 in bgcoder
+using System;
+using System.Numerics;
+
+class ConsoleApplicationString
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        int position = 0;
+        BigInteger product10 = 1;
+        BigInteger productAfter10 = 1;
+
+        while (input != "END")
+        {
+            if (position % 2 == 0 && input != "0")
+            {
+                BigInteger product = 1;
+                foreach (var symbol in input)
+                {
+                    if (symbol != '0')
+                    {
+                        product *= symbol - '0';
+                    }
+                }
+
+                if (position < 10)
+                {
+                    product10 *= product;
+                }
+                else //if (position >= 10)
+                {
+                    productAfter10 *= product;
+                }
+            }
+
+            input = Console.ReadLine();
+            position++;
+        }
+
+        Console.WriteLine(product10);
+        if (position > 9)
+        {
+            Console.WriteLine(productAfter10);
+        }
+
+    }
+}
