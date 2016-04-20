ccd5a7472e88fa93a0875628ecb00ddc93cd40b5
petyakostova <petya_kostova_@abv.bg>
2016-04-20 05:48:06 +0300

    solve Diamond-Trolls/04. Diamond-Trolls/DiamondTrolls.cs

--- C#/C# 1 Contests/4/Diamond-Trolls/04. Diamond-Trolls/DiamondTrolls.cs
+++ C#/C# 1 Contests/4/Diamond-Trolls/04. Diamond-Trolls/DiamondTrolls.cs
@@ -1,11 +1,38 @@
 ﻿using System;
 
-namespace _04.Diamond_Trolls
+class DiamondTrolls
 {
-    class DiamondTrolls
+    static void Main()
     {
-        static void Main(string[] args)
+        int n = int.Parse(Console.ReadLine());
+
+        int width = n * 2 + 1;
+        int height = (n - 3) / 2 * 3 + 6;
+        int top = n;
+
+        // part 1
+        Console.WriteLine("{0}{1}{0}", new string('.', (width - top) / 2), new string('*', top));
+
+        // part 2
+        for (int i = 0; i < height - n - 2; i++)
         {
+            Console.WriteLine("{0}*{1}*{1}*{0}", new string('.', height - n - 2 - i), new string('.', height - n - 2 + i));
         }
+
+        // part 3
+        Console.WriteLine(new string('*', width));
+
+        // part 4
+        for (int i = 0; i < n - 2; i++)
+        {
+            Console.WriteLine("{0}*{1}*{1}*{0}", new string('.', 1 + i), new string('.', n - 2 - i));
+        }
+
+        // part 5
+        Console.WriteLine("{0}***{0}", new string('.', n - 1));
+
+        // part 6
+        Console.WriteLine("{0}*{0}", new string('.', n));
+
     }
 }
