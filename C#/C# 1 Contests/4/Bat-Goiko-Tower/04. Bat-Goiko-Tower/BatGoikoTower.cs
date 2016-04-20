17ef33364f7778ba5e3befe97b20e7d79cfe4dd5
petyakostova <petya_kostova_@abv.bg>
2016-04-20 04:19:43 +0300

    solve Bat-Goiko-Tower/04. Bat-Goiko-Tower/BatGoikoTower.cs

--- C#/C# 1 Contests/4/Bat-Goiko-Tower/04. Bat-Goiko-Tower/BatGoikoTower.cs
+++ C#/C# 1 Contests/4/Bat-Goiko-Tower/04. Bat-Goiko-Tower/BatGoikoTower.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 
-namespace _04.Bat_Goiko_Tower
+class BatGoikoTower
 {
-    class BatGoikoTower
+    static void Main()
     {
-        static void Main(string[] args)
+        int height = int.Parse(Console.ReadLine());
+
+        // part 1
+        Console.WriteLine("{0}/\\{0}", new string('.', height - 1));
+
+        // part 2
+        int dashPut = 0;
+        int dashPutAfter = 2;
+        for (int i = 0; i < height - 1; i++)
         {
+            if (i == dashPut)
+            {
+                Console.WriteLine("{0}/{1}\\{0}", new string('.', height - 2 - i), new string('-', 2 + 2 * i));
+                dashPut += dashPutAfter;
+                dashPutAfter++;
+            }
+            else
+            {
+                Console.WriteLine("{0}/{1}\\{0}", new string('.', height - 2 - i), new string('.', 2 + 2 * i));
+            }
         }
+
     }
 }
