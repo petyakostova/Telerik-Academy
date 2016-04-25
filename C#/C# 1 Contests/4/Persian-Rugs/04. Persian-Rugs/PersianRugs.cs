68ddc6f9282ec9e1a82d834e161b384bd31c7442
petyakostova <petya_kostova_@abv.bg>
2016-04-25 03:30:35 +0300

    modify Persian-Rugs/04. Persian-Rugs/PersianRugs.cs for 55/100 in bgcoder - print without the inside triangles

--- C#/C# 1 Contests/4/Persian-Rugs/04. Persian-Rugs/PersianRugs.cs
+++ C#/C# 1 Contests/4/Persian-Rugs/04. Persian-Rugs/PersianRugs.cs
@@ -1,11 +1,39 @@
 ﻿using System;
 
-namespace _04.Persian_Rugs
+class PersianRugs
 {
-    class PersianRugs
+    static void Main()
     {
-        static void Main(string[] args)
+        int n = int.Parse(Console.ReadLine());
+        int d = int.Parse(Console.ReadLine());
+        int width = 2 * n + 1;
+
+        // part 1
+        for (int row = 0; row < n; row++)
         {
+            Console.Write(new string('#', row));
+            Console.Write("\\");
+            Console.Write(new string(' ', width - 2 * row - 2));
+            Console.Write("/");
+            Console.Write(new string('#', row));
+
+            Console.WriteLine();
         }
+
+        // part 2 (middle line)
+        Console.WriteLine(new string('#', n) + 'X' + new string('#', n));
+
+        // part 3 - symmetric to part 1
+        for (int row = n - 1; row >= 0; row--)
+        {
+            Console.Write(new string('#', row));
+            Console.Write("/");
+            Console.Write(new string(' ', width - 2 * row - 2));
+            Console.Write("\\");
+            Console.Write(new string('#', row));
+
+            Console.WriteLine();
+        }
+
     }
 }
