30474e5c78f89598d91b7e8b09f5abc4e938102b
petyakostova <petya_kostova_@abv.bg>
2016-04-25 04:27:14 +0300

    solve Cube/04. Cube/Cube.cs

--- C#/C# 1 Contests/4/Cube/04. Cube/Cube.cs
+++ C#/C# 1 Contests/4/Cube/04. Cube/Cube.cs
@@ -4,7 +4,31 @@ class Cube
 {
     static void Main()
     {
-        
+        int n = int.Parse(Console.ReadLine());
+
+        // part 1
+        Console.WriteLine(new string(' ', n - 1) + new string(':', n));
+
+        // part 2
+        Console.WriteLine(new string(' ', n - 2) + ':' + new string('/', n - 2) + new string(':', 2));
+
+        // part 3
+        for (int i = 0; i < n - 3; i++)
+        {
+            Console.WriteLine("{0}{1}{2}{1}{3}{1}", new string(' ', n - 3 - i), new string(':', 1), new string('/', n - 2), new string('X', 1 + i));
+        }
+
+        // part 4
+        Console.WriteLine(new string(':', n) + new string('X', n - 2) + ':');
+
+        // part 5
+        for (int i = 0; i < n - 2; i++)
+        {
+            Console.WriteLine("{0}{1}{0}{2}{0}{3}", new string(':', 1), new string(' ', n - 2), new string('X', n - 3 - i), new string(' ', 1 + i));
+        }
+
+        // part 6
+        Console.WriteLine(new string(':', n) + new string(' ', n - 1));
 
     }
 }
