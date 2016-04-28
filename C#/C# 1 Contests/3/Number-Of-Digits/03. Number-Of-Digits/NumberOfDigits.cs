ff93861adba307eb860033d9793a66daf2f6bbe1
petyakostova <petya_kostova_@abv.bg>
2016-04-29 02:33:41 +0300

    solve Number-Of-Digits/03. Number-Of-Digits/NumberOfDigits.cs and add Batman/README.md

--- C#/C# 1 Contests/3/Number-Of-Digits/03. Number-Of-Digits/NumberOfDigits.cs
+++ C#/C# 1 Contests/3/Number-Of-Digits/03. Number-Of-Digits/NumberOfDigits.cs
@@ -4,6 +4,15 @@ class NumberOfDigits
 {
     static void Main()
     {
+        int numberOfPages = int.Parse(Console.ReadLine());
 
+        int digitsInPageNumbers = 0;
+
+        for (int i = 1; i <= numberOfPages; i++)
+        {
+            digitsInPageNumbers += i.ToString().Length;
+        }
+
+        Console.WriteLine(digitsInPageNumbers);
     }
 }
