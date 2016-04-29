20365b7d4976f257deabe69645e7d092aa2c29fa
petyakostova <petya_kostova_@abv.bg>
2016-04-29 17:48:54 +0300

    solve Sum-Of-Even-Divisors/03. Sum-Of-Even-Divisors/SumOfEvenDivisors.cs and Sum-Of-Odd-Divisors/03. Sum-Of-Odd-Divisors/Properties/AssemblyInfo.cs

---
+++ C#/C# 1 Contests/3/Sum-Of-Even-Divisors/03. Sum-Of-Even-Divisors/SumOfEvenDivisors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class SumOfEvenDivisors
+{
+    static void Main()
+    {
+        int startInterval = int.Parse(Console.ReadLine());
+        int endInterval = int.Parse(Console.ReadLine());
+        long sum = 0;
+
+        for (int i = startInterval; i <= endInterval; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % j == 0 && j % 2 == 0)
+                {
+                    sum += j;
+                }
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
