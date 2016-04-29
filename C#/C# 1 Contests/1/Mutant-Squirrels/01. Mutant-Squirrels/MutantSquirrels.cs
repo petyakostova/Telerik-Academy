8dab4b516984b2dbddd02245aaec1fb6efe9cbe3
petyakostova <petya_kostova_@abv.bg>
2016-04-29 15:44:40 +0300

    solve Mutant-Squirrels/01. Mutant-Squirrels/MutantSquirrels.cs

---
+++ C#/C# 1 Contests/1/Mutant-Squirrels/01. Mutant-Squirrels/MutantSquirrels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class MutantSquirrels
+{
+    static void Main()
+    {
+        const ulong multiplier = 376439;
+        const double divisor = 7.00;
+
+        ulong trees = ulong.Parse(Console.ReadLine());
+        uint branchesOnTree = uint.Parse(Console.ReadLine());
+        uint squirrelsOnBranch = uint.Parse(Console.ReadLine());
+        uint averageTailsForSquirrel = uint.Parse(Console.ReadLine());
+
+        ulong tails = trees * branchesOnTree * squirrelsOnBranch * averageTailsForSquirrel;
+        double result = 0;
+
+        if (tails % 2 == 0)
+        {
+            result = tails * multiplier;
+        }
+        else
+        {
+            result = tails / divisor;
+        }
+
+        Console.WriteLine("{0:F3}", result);
+    }
+}
