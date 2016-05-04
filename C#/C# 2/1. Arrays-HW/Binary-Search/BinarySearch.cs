fbf3658eb41974ee4a2eb99d5a797a63104c7a6a
petyakostova <petya_kostova_@abv.bg>
2016-05-04 16:16:08 +0300

    update Binary-Search/BinarySearch.cs

--- C#/C# 2/1. Arrays-HW/Binary-Search/BinarySearch.cs
+++ C#/C# 2/1. Arrays-HW/Binary-Search/BinarySearch.cs
@@ -1,6 +1,13 @@
 ﻿/*  Problem 11. Binary search
-    Write a program that finds the index of given element in a sorted array of integers by using the Binary search algorithm.
- */
+    Write a program that finds the index of given element X in a sorted array of N integers by using the Binary search algorithm.
+    Input:
+            On the first line you will receive the number N
+            On the next N lines the numbers of the array will be given
+            On the last line you will receive the number X
+    Output:
+            Print the index where X is in the array
+                If there is more than one occurence print the first one
+                If there are no occurences print -1                              */
 
 using System;
 
@@ -8,41 +15,31 @@ class BinarySearch
 {
     static void Main()
     {
-        // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-        // Input array
-        Console.WriteLine("Enter an array of integer elements on a single line, separated by commas:");
-        string input = Console.ReadLine();
-
-        // Split the elements into an array
-        string[] inputElements = input.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-        // Fill an integer array from string array
-        int[] array = new int[inputElements.Length];
-        for (int i = 0; i < inputElements.Length; i++)
+        // Input 
+        int n = int.Parse(Console.ReadLine());
+        int[] arrayNums = new int[n];
+        for (int i = 0; i < n; i++)
         {
-            array[i] = int.Parse(inputElements[i]);
+            arrayNums[i] = int.Parse(Console.ReadLine());
         }
-
-        // Input Number
-        Console.WriteLine("Enter the element, whose index You want to find: ");
-        int element = int.Parse(Console.ReadLine());
+        int element = int.Parse(Console.ReadLine()); //the element, whose index You want to find
 
         // Binary search works only on sorted arrays.
-        Array.Sort(array);
+        Array.Sort(arrayNums);
 
         int left = 0;
-        int right = array.Length - 1;
+        int right = arrayNums.Length - 1;
         int middle = 0;
         int index = -1;
-      
+
         while (right >= left)
         {
             middle = (left + right) / 2;
-            if (array[middle] < element)
+            if (arrayNums[middle] < element)
             {
                 left = middle + 1;
             }
-            else if (array[middle] > element)
+            else if (arrayNums[middle] > element)
             {
                 right = middle - 1;
             }
@@ -53,14 +50,7 @@ class BinarySearch
             }
         }
 
-        if (index != -1)
-        {
-            Console.WriteLine("The index of the given element is: {0}", index);
-        }
-        else
-        {
-            Console.WriteLine("There is no number {0} in the array.", element);
-        }
-        
+        Console.WriteLine(index);
+
     }
 }
