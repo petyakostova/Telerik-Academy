fa00ea6fb323bc1b01ad57c5cacf5fdc6fc3f1d3
petyakostova <petya_kostova_@abv.bg>
2016-05-04 22:45:44 +0300

    update Merge-Sort/MergeSort.cs

--- C#/C# 2/1. Arrays-HW/Merge-Sort/MergeSort.cs
+++ C#/C# 2/1. Arrays-HW/Merge-Sort/MergeSort.cs
@@ -10,15 +10,75 @@ using System;
 
 class MergeSort
 {
+    static int[] arrayNums;
+
     static void Main()
     {
         int n = int.Parse(Console.ReadLine());
-        int[] arrayNums = new int[n];
+        arrayNums = new int[n];
         for (int i = 0; i < n; i++)
         {
             arrayNums[i] = int.Parse(Console.ReadLine());
         }
 
+        MergeSort(0, arrayNums.Length - 1);
 
+        for (int i = 0; i < arrayNums.Length; i++)
+        {
+            Console.WriteLine(arrayNums[i]);
+        }
     }
+
+    static void MergeSort(int start, int end)
+    {
+        // Array with 1 element
+        if (start >= end) return;
+
+        // Define a middle of the array
+        int middle = (start + end) / 2;
+
+        MergeSort(start, middle);
+        MergeSort(middle + 1, end);
+
+        CompareAndSort(start, middle, end);
+    }
+
+    static void CompareAndSort(int start, int middle, int end)
+    {
+        int[] sortedArr = new int[arrayNums.Length];
+
+        int valuesLocalArr = start; // for saving the values of the local array, which always starts from start
+        int leftArrStart = start; // for the beginning of the left array
+        int middleArrStart = middle + 1; // where starts the right sub-array <=> the left range ot the right sub-array
+
+        //inserting the smaller numbers
+        while (leftArrStart <= middle && middleArrStart <= end)
+        {
+            if (arrayNums[leftArrStart] > arrayNums[middleArrStart])
+            {
+                sortedArr[valuesLocalArr++] = arrayNums[middleArrStart++];
+            }
+            else
+            {
+                sortedArr[valuesLocalArr++] = arrayNums[leftArrStart++];
+            }
+        }
+
+        //inserting other numbers
+        while (leftArrStart <= middle)
+        {
+            sortedArr[valuesLocalArr++] = arrayNums[leftArrStart++];
+        }
+
+        while (middleArrStart <= end)
+        {
+            sortedArr[valuesLocalArr++] = arrayNums[middleArrStart++];
+        }
+
+        for (int i = start; i <= end; i++)
+        {
+            arrayNums[i] = sortedArr[i];
+        }
+    }
+
 }
--- C#/C# 2/1. Arrays-HW/Merge-Sort/README.md
+++ C#/C# 2/1. Arrays-HW/Merge-Sort/README.md
@@ -28,3 +28,4 @@ Write a program that sorts an array of integers using the [Merge sort](http://en
 
 ## More info for the solution:
 - [Merge sort](https://en.wikipedia.org/wiki/Merge_sort)
+- [Merge Sort (сортиране чрез сливане)](https://binevv.wordpress.com/2014/02/17/merge-sort-%D1%81%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%B0%D0%BD%D0%B5-%D1%87%D1%80%D0%B5%D0%B7-%D1%81%D0%BB%D0%B8%D0%B2%D0%B0%D0%BD%D0%B5/)
