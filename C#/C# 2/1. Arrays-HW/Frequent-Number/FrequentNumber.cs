d803e0154f5d68e6af04117a90759d916445ab7a
petyakostova <petya_kostova_@abv.bg>
2016-05-04 06:09:44 +0300

    solve Maximal-Sum/MaximalSum.cs and Frequent-Number/FrequentNumber.cs

--- C#/C# 2/1. Arrays-HW/Frequent-Number/FrequentNumber.cs
+++ C#/C# 2/1. Arrays-HW/Frequent-Number/FrequentNumber.cs
@@ -1,9 +1,7 @@
 ﻿/*  Problem 9. Frequent number
-    Write a program that finds the most frequent number in an array.
-    Example:
-        input 	                                    result
-        4, 1, 1, 4, 2, 3, 4, 4, 1, 2, 4, 9, 3 	    4 (5 times)
- */
+    Write a program that finds the most frequent number in an array of N elements.
+    Print the most frequent number and how many time it is repeated.
+        Output should be REPEATING_NUMBER (REPEATED_TIMES times)                 */
 
 using System;
 
@@ -11,53 +9,48 @@ class FrequentNumber
 {
     static void Main()
     {
-        //Input
-        Console.WriteLine("Enter an array of integer elements on a single line, separated by commas:");
-        string input = Console.ReadLine();
-
-        //Split the elements into an array
-        string[] inputElements = input.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-        //Fill an integer array from string array
-        int[] arrayNums = new int[inputElements.Length];
-        for (int i = 0; i < inputElements.Length; i++)
+        //input
+        int n = int.Parse(Console.ReadLine());
+        int[] arrayNums = new int[n];
+        for (int i = 0; i < n; i++)
         {
-            arrayNums[i] = int.Parse(inputElements[i]);
+            arrayNums[i] = int.Parse(Console.ReadLine());
         }
-
+        
         // sorting the numbers in ascending order, so the elements with same value will be placed next to each other
         Array.Sort(arrayNums);
 
         // find the longest sequence of neighbor equal elements
-        int currentLength = 1;
-        int maxLength = 0;
-        int elementBestLength = 0;
+        int count = 1;
+        int repeatCountBest = 0; // repeated times
+        int repeatingNumBest = 0;
 
         for (int i = 0; i < arrayNums.Length - 1; i++)
         {
             if (arrayNums[i] == arrayNums[i + 1])
             {
-                currentLength++;
+                count++;
             }
             else
             {
-                if (currentLength > maxLength)
+                if (count > repeatCountBest)
                 {
-                    maxLength = currentLength;
-                    elementBestLength = arrayNums[i];
+                    repeatCountBest = count;
+                    repeatingNumBest = arrayNums[i];
                 }
-                currentLength = 1;
+                count = 1;
             }
         }
+
         // in case when the last sequence is the maximal sequence of equal elements
-        if (currentLength > maxLength)
+        if (count > repeatCountBest)
         {
-            maxLength = currentLength;
-            elementBestLength = arrayNums[arrayNums.Length - 1];
+            repeatCountBest = count;
+            repeatingNumBest = arrayNums[arrayNums.Length - 1];
         }
 
         // output
-        Console.WriteLine("{0} ({1} times)", elementBestLength, maxLength);
+        Console.WriteLine("{0} ({1} times)", repeatingNumBest, repeatCountBest);
         
     }
 }
--- C#/C# 2/1. Arrays-HW/Maximal-Sum/MaximalSum.cs
+++ C#/C# 2/1. Arrays-HW/Maximal-Sum/MaximalSum.cs
@@ -1,10 +1,6 @@
 ﻿/*  Problem 8. Maximal sum
-    Write a program that finds the sequence of maximal sum in given array.
-    Example:
-        input 	                                result
-        2, 3, -6, -1, 2, -1, 6, 4, -8, 8 	    2, -1, 6, 4
-    Can you do it with only one loop (with single scan through the elements of the array)?
- */
+    Write a program that finds the maximal sum of consecutive elements in a given array of N numbers.
+    Can you do it with only one loop (with single scan through the elements of the array)?             */
 
 using System;
 
@@ -12,22 +8,16 @@ class MaximalSum
 {
     static void Main()
     {
-        //Input
-        Console.WriteLine("Enter an array of integer elements on a single line, separated by commas:");
-        string input = Console.ReadLine();
-
-        //Split the elements into an array
-        string[] inputElements = input.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-        //Fill an integer array from string array
-        int[] arrayNums = new int[inputElements.Length];
-        for (int i = 0; i < inputElements.Length; i++)
+        // input
+        int n = int.Parse(Console.ReadLine());
+        int[] arrayNums = new int[n];
+        for (int i = 0; i < n; i++)
         {
-            arrayNums[i] = int.Parse(inputElements[i]);
+            arrayNums[i] = int.Parse(Console.ReadLine());
         }
 
-        int startIndex = 0, currentSum = 0;
-        int bestStart = 0, bestEnd = 0, maxSum = int.MinValue;
+        int startIndex = 0, bestStart = 0, bestEnd = 0;
+        int currentSum = 0, maxSum = int.MinValue;
 
         for (int i = 0; i < arrayNums.Length; i++)
         {
@@ -36,7 +26,7 @@ class MaximalSum
                 startIndex = i;
                 currentSum = 0;
             }
-            
+
             currentSum += arrayNums[i];
 
             if (currentSum > maxSum)
@@ -46,13 +36,14 @@ class MaximalSum
                 bestEnd = i;
             }
         }
-      
-        Console.Write("Sequence of maximal sum: ");
-        for (int i = bestStart; i <= bestEnd; i++)
-        {
-            Console.Write(i != bestEnd ? arrayNums[i] + ", " : arrayNums[i] + "\n");
-        }
-        Console.WriteLine("Maximal sum: {0}", maxSum);
-        
+
+        Console.WriteLine(maxSum);
+
+        //Print the sequence of maximal sum
+        //for (int i = bestStart; i <= bestEnd; i++)
+        //{
+        //    Console.Write(i != bestEnd ? arrayNums[i] + ", " : arrayNums[i] + "\n");
+        //}        
+
     }
 }
