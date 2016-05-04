7bd68f7518d10e225b6086c61402830fe8bd42f4
petyakostova <petya_kostova_@abv.bg>
2016-05-04 03:50:10 +0300

    update Maximal-Increasing-Sequence/MaximalIncreasingSequence.cs for 10/100

--- C#/C# 2/1. Arrays-HW/Maximal-Increasing-Sequence/MaximalIncreasingSequence.cs
+++ C#/C# 2/1. Arrays-HW/Maximal-Increasing-Sequence/MaximalIncreasingSequence.cs
@@ -1,9 +1,10 @@
 ﻿/*  Problem 5. Maximal increasing sequence
-    Write a program that finds the maximal increasing sequence in an array.
-    Example:
-        input 	                    result
-        3, 2, 3, 4, 2, 2, 4 	    2, 3, 4
- */
+    Write a program that finds the length of the maximal increasing sequence in an array of N integers.
+    Input:
+            On the first line you will receive the number N
+            On the next N lines the numbers of the array will be given
+    Output: Print the length of the maximal increasing sequence                 */
+
 // Note: The increasing elements are consecutively placed.
 
 using System;
@@ -13,26 +14,20 @@ class MaximalIncreasingSequence
     static void Main()
     {
         //Input
-        Console.WriteLine("Enter an array of integer elements on a single line, separated by commas:");
-        string input = Console.ReadLine();
-
-        //Split the elements into an array
-        string[] inputElements = input.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-        //Fill an integer array from string array
-        int[] elementsArray = new int[inputElements.Length];
-        for (int i = 0; i < inputElements.Length; i++)
+        int n = int.Parse(Console.ReadLine());
+        int[] arrayNums = new int[n];
+        for (int i = 0; i < n; i++)
         {
-            elementsArray[i] = int.Parse(inputElements[i]);
+            arrayNums[i] = int.Parse(Console.ReadLine());
         }
-
+        
         int currentLength = 1;
         int maxLength = 0;
-        int endIndexBestLength = 0;
+        //int endIndexBestLength = 0;
 
-        for (int i = 0; i < elementsArray.Length - 1; i++)
+        for (int i = 0; i < arrayNums.Length - 1; i++)
         {
-            if (elementsArray[i] < elementsArray[i + 1])
+            if (arrayNums[i] <= arrayNums[i + 1])
             {
                 currentLength++;
             }
@@ -41,23 +36,25 @@ class MaximalIncreasingSequence
                 if (currentLength > maxLength)
                 {
                     maxLength = currentLength;
-                    endIndexBestLength = i;
+                    //endIndexBestLength = i;
                 }
                 currentLength = 1;
             }
         }
 
-        // in case when the last sequence is the maximal sequence of equal elements
+        // in case when the last sequence is the maximal increasing sequence
         if (currentLength > maxLength)
         {
             maxLength = currentLength;
-            endIndexBestLength = elementsArray.Length - 1;
+            //endIndexBestLength = arrayNums.Length - 1;
         }
 
-        for (int i = endIndexBestLength - maxLength + 1; i <= endIndexBestLength; i++)
-        {
-            Console.Write(i != endIndexBestLength ? elementsArray[i] + ", " : elementsArray[i] + "\n");
-        }       
+        Console.WriteLine(maxLength);
+
+        //for (int i = endIndexBestLength - maxLength + 1; i <= endIndexBestLength; i++)
+        //{
+        //    Console.Write(i != endIndexBestLength ? arrayNums[i] + ", " : arrayNums[i] + "\n");
+        //}       
 
     }
 }
