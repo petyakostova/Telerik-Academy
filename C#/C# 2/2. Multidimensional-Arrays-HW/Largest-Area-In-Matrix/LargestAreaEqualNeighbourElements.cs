69f7705cf47daa4120b5d5c49da801ad83aa3aee
petyakostova <petya_kostova_@abv.bg>
2016-05-10 00:51:59 +0300

    modify Largest-Area-In-Matrix/LargestAreaEqualNeighbourElements.cs for 20 / 100 in bgcoder and 80 time limits

--- C#/C# 2/2. Multidimensional-Arrays-HW/Largest-Area-In-Matrix/LargestAreaEqualNeighbourElements.cs
+++ C#/C# 2/2. Multidimensional-Arrays-HW/Largest-Area-In-Matrix/LargestAreaEqualNeighbourElements.cs
@@ -2,7 +2,7 @@
     Write a program that finds the largest area of equal neighbour elements in a rectangular matrix and prints its size.
     Input: On the first line you will receive the numbers N and M separated by a single space
     On the next N lines there will be M numbers separated with spaces - the elements of the matrix
-    Output: Print the size of the lasrgest area of equal neighbour elements
+    Output: Print the size of the largest area of equal neighbour elements
     Constraints: 3 <= N, M <= 1024
     Sample test:
                     Input 	        Output
@@ -20,13 +20,30 @@ class LargestAreaEqualNeighbourElements
 {
     static int answer = 0;
     static int absolutemax = 0;
-    static int[,] matrix = new int[,] { { 1, 3, 2, 2, 2, 4 },       // stand for check
-                                        { 3, 3, 3, 2, 4, 4 }, 
-                                        { 4, 3, 1, 2, 3, 3 }, 
-                                        { 4, 3, 1, 3, 3, 1 }, 
-                                        { 4, 3, 3, 3, 1, 1 } };
+    static int[,] matrix;
+
     static void Main()
     {
+        // Read the matrix dimensions
+        string[] sizes = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        int rows = int.Parse(sizes[0]);
+        int cols = int.Parse(sizes[1]);
+
+        // Create (Allocate) the matrix
+        matrix = new int[rows, cols];
+
+        // Enter the matrix elements
+        for (int row = 0; row < rows; row++)
+        {
+            string[] inputRows = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int col = 0, count = 0; col < cols; col++, count++)
+            {
+                matrix[row, col] = int.Parse(inputRows[count]);
+            }
+        }
+
+        // Find the largest area of equal neighbour elements
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
             for (int j = 0; j < matrix.GetLength(1); j++)
@@ -35,13 +52,15 @@ class LargestAreaEqualNeighbourElements
                 answer = 0;
             }
         }
+
+        // Output
         Console.WriteLine(absolutemax);
     }
 
     private static void FindTheArea(int i, int j, int currelement)
     {
         //returns if we are out of the matrix or the element is not the same
-        if ((currelement == 0) || (i < 0) || (i >= matrix.GetLength(0)) || (j < 0) || (j >= matrix.GetLength(1))) 
+        if ((currelement == 0) || (i < 0) || (i >= matrix.GetLength(0)) || (j < 0) || (j >= matrix.GetLength(1)))
         {
             return;
         }
