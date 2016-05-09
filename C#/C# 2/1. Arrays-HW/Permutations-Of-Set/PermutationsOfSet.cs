d8e622fe089c218231926d5645bea005111a7e55
petyakostova <petya_kostova_@abv.bg>
2016-05-09 16:16:43 +0300

    add Permutations-Of-Set/PermutationsOfSet.cs and README.md

---
+++ C#/C# 2/1. Arrays-HW/Permutations-Of-Set/PermutationsOfSet.cs
@@ -0,0 +1,21 @@
+﻿/* Problem 19. Permutations of set
+   Write a program that reads a number N and generates and prints all the permutations of the numbers [1...N].
+   Sample tests
+    N 	    result
+    3 	    {1, 2, 3}
+            {1, 3, 2}
+            {2, 1, 3}
+            {2, 3, 1}
+            {3, 1, 2}
+            {3, 2, 1}           */
+
+using System;
+
+class PermutationsOfSet
+{
+    static void Main()
+    {
+
+
+    }
+}
