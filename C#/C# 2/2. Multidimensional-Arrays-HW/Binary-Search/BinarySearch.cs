96e95e3646414787c6fac98bc364d221fb0768db
petyakostova <petya_kostova_@abv.bg>
2016-05-11 20:01:51 +0300

    solve Binary-Search/BinarySearch.cs

--- C#/C# 2/2. Multidimensional-Arrays-HW/Binary-Search/BinarySearch.cs
+++ C#/C# 2/2. Multidimensional-Arrays-HW/Binary-Search/BinarySearch.cs
@@ -11,9 +11,9 @@ class BinarySearch
     {
         Console.Write("Enter an integer number for the length of the array N: ");
         int n = int.Parse(Console.ReadLine());
-        
+
         Console.Write("Enter an integer number K: ");
-        int K = int.Parse(Console.ReadLine());
+        int k = int.Parse(Console.ReadLine());
 
         int[] array = new int[n + 1];
         Console.WriteLine("Enter {0} numbers to array:", n);
@@ -21,31 +21,33 @@ class BinarySearch
         {
             array[i] = int.Parse(Console.ReadLine());
         }
-                
-        array[n] = K;
+
+        array[n] = k;
 
         // Binary search works only on sorted arrays.
         Array.Sort(array);
 
-        int indexK = Array.BinarySearch(array, K);
+        /*BinarySearch() returns the index of the specified value in the specified array, if value is found. 
+        If value is not found and value is less than one or more elements in array, a negative number 
+        which is the bitwise complement of the index of the first element that is larger than value. 
+        If value is not found and value is greater than any of the elements in array, a negative number 
+        which is the bitwise complement of (the index of the last element plus 1).*/
+        int indexK = Array.BinarySearch(array, k);
 
-        if (indexK == n)
-        {
-            Console.WriteLine("\nThe largest number in the array which is <= {0} is: \n{1}", K, array[n - 1]);
-        }
-        else if (array[indexK] == array[indexK + 1])
-        {
-            Console.WriteLine("\nThe largest number in the array which is <= {0} is: \n{1}", K, array[indexK]);
-        }
-        else if (indexK == 0)
+        //if (array[indexK] == array[indexK + 1])
+        //{
+        //    Console.WriteLine("\nThe largest number in the array which is <= {0} is: \n{1}", k, array[indexK]);
+        //}
+        //else 
+        if (indexK == 0)
         {
             Console.WriteLine(
-                "\nThere is no smaller or equal to K = {0} number in the array.\nAll the elements are larger than {0}.", K);
+                "\nThere is no smaller or equal to K = {0} number in the array.\nAll the elements are larger than {0}.", k);
         }
         else
         {
-            Console.WriteLine(array[indexK - 1]);
-        }      
-        
+            Console.WriteLine("\nThe largest number in the array which is <= {0} is: {1}", k, array[indexK - 1]);
+        }
+
     }
 }
