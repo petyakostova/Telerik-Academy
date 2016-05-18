84ecd618509d7e1007a01140590580c8fae5f177
petyakostova <petya_kostova_@abv.bg>
2016-05-19 02:29:17 +0300

    solve Workdays/Workdays.cs

--- C#/C# 2/5. Using-Classes-And-Objects-HW/Workdays/Workdays.cs
+++ C#/C# 2/5. Using-Classes-And-Objects-HW/Workdays/Workdays.cs
@@ -1,30 +1,42 @@
-﻿/*  Problem 5. Workdays
-    Write a method that calculates the number of workdays between today and given date, passed as parameter.
+﻿/*  5. Workdays
+    Write a method that calculates the number of workdays between today and a given date, passed as parameter.
     Consider that workdays are all days from Monday to Friday 
-    except a fixed list of public holidays specified preliminary as array.
- */
+    except a fixed list of public holidays specified preliminary as array. */
+
+// This Solution works in the range of the current year
 
 using System;
 using System.Collections.Generic;
+
 class Workdays
 {
     static List<DateTime> publicHolydays;
 
     static void Main()
     {
-        publicHolydays = new List<DateTime>()
-        {new DateTime(2015, 1, 1),new DateTime(2015, 3, 2),new DateTime(2015, 3, 3),
-        new DateTime(2015, 4, 10), new DateTime(2015, 4, 11),new DateTime(2015, 4, 12),new DateTime(2015, 4, 13),
-        new DateTime(2015, 5, 1),new DateTime(2015, 5, 6),new DateTime(2015, 9, 21), new DateTime(2015, 9, 22),
-        new DateTime(2015, 12, 24),new DateTime(2015, 12, 25),new DateTime(2015, 12, 26),new DateTime(2015, 12, 31),};
+        int currentYear = DateTime.Now.Year;
 
-        DateTime currentDate = DateTime.Today;  // gets the current date
-        //Console.WriteLine(Convert.ToString(currentDate.ToShortDateString()));   // prints only the current date
+        // list of public holidays
+        publicHolydays = new List<DateTime>() { new DateTime(currentYear, 1, 1),
+            new DateTime(currentYear, 3, 2), new DateTime(currentYear, 3, 3),
+            new DateTime(currentYear, 4, 10), new DateTime(currentYear, 4, 11),
+            new DateTime(currentYear, 4, 12), new DateTime(currentYear, 4, 13),
+            new DateTime(currentYear, 5, 1), new DateTime(currentYear, 5, 6),
+            new DateTime(currentYear, 9, 21), new DateTime(currentYear, 9, 22),
+            new DateTime(currentYear, 12, 24), new DateTime(currentYear, 12, 25),
+            new DateTime(currentYear, 12, 26), new DateTime(currentYear, 12, 31),};
+
+        // gets the current date
+        DateTime currentDate = DateTime.Today;
+        // gets the current date - other way
+        //DateTime currentDate = DateTime.Now.Date;
+        //Console.WriteLine(Convert.ToString(currentDate.ToShortDateString())); // prints only the current date
 
         Console.Write("Enter a date in format (dd.mm.yyyy): ");
         DateTime date = Convert.ToDateTime(Console.ReadLine());
-        //Console.WriteLine(Convert.ToString(date.ToShortDateString()));  // prints only the input date        
+        //Console.WriteLine(Convert.ToString(date.ToShortDateString())); // prints only the input date        
 
+        // Output
         Console.WriteLine(CountWorkdays(currentDate, date));
     }
 
@@ -33,22 +45,22 @@ class Workdays
         int workDays = 0;
         int holydays = 0;
         int weekends = 0;
-        
+
         while (startDate <= endDate)
         {
             if (publicHolydays.Contains(startDate))
             {
                 holydays++;
             }
-            else if ((int)startDate.DayOfWeek == 6 || (int)startDate.DayOfWeek == 0)    // 6 => Suturday; 0 => Sunday
+            else if ((int)startDate.DayOfWeek == 6 || (int)startDate.DayOfWeek == 0) // 6=>Saturday; 0=>Sunday   
             {
                 weekends++;
             }
             else
             {
                 workDays++;
-            }               
- 
+            }
+
             startDate = startDate.AddDays(1);
         }
 
@@ -58,4 +70,5 @@ class Workdays
         Workdays: {2}
         ", holydays, weekends, workDays);
     }
+
 }
