8e8582efbb2ccb5ac048de9b6583f3f1086219ad
petyakostova <petya_kostova_@abv.bg>
2016-05-18 23:20:52 +0300

    add Binary-Short/ShortToBinaryConvertion.cs

---
+++ C#/C# 2/4. Numeral-Systems-HW/Binary-Short/ShortToBinaryConvertion.cs
@@ -0,0 +1,39 @@
+﻿/*  Problem 8. Binary short
+    Write a program that shows the binary representation 
+    of given 16-bit signed integer number N (the C# type short).
+    Input: On the only line you will receive a decimal number - N
+    Output: Print the its binary representation on a single line. There should be exactly 16 digits of output.
+    Constraints: -215 <= N < 215
+    Sample tests:
+                    Input 	    Output
+                    11 	        0000000000001011
+                    -11 	    1111111111110101                       
+                    -1          1111111111111111
+                    500         0000000111110100
+                    -56         1111111111001000    */
+
+using System;
+
+class ShortToBinaryConvertion
+{
+    static void Main()
+    {
+        short shortNumber = short.Parse(Console.ReadLine());
+
+        Console.WriteLine(ShortToBinary(shortNumber));
+    }
+
+    private static string ShortToBinary(int number)
+    {
+        string binaryNumber = string.Empty;
+
+        for (int i = 15; i >= 0; i--)
+        {
+            binaryNumber = ((number % 2) & 1) + binaryNumber;
+            number >>= 1;
+            //if (i % 4 == 0 && i != 0) binaryNumber = " " + binaryNumber;  // for better print
+        }
+
+        return binaryNumber;
+    }
+}
--- C#/C# 2/4. Numeral-Systems-HW/One-System-To-Any-Other/OneSystemToAnyOther.cs
+++ C#/C# 2/4. Numeral-Systems-HW/One-System-To-Any-Other/OneSystemToAnyOther.cs
@@ -1,6 +1,16 @@
-﻿/*  Problem 7. One system to any other
-    Write a program to convert from any numeral system of given base s to any other numeral system of base d (2 ≤ s, d ≤ 16).
- */
+﻿/*  7. One system to any other
+    Write a program to convert the number N 
+    from any numeral system of given base s to any other numeral system of base d.
+    Input: On the first line you will receive the number s.
+           On the second line you will receive a number in base s - N. There will not be leading zeros
+           On the third line you will receive the number d
+    Output: Print N in base d. There should not be leading zeros. Use uppercase letters
+    Constraints: 2 <= s, d <= 16;       1 <= N <= 1018
+    Sample tests:
+                    Input 	    Output
+                    13          21
+                    16
+                    9 	                    */
 
 using System;
 using System.Linq;
