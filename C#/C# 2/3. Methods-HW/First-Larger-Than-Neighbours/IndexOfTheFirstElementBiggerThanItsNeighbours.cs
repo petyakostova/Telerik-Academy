c4dcabcf6c9b16b4974d58c49ea7062e3da26ebd
petyakostova <petya_kostova_@abv.bg>
2016-05-18 14:32:41 +0300

    solve task 6, modify task 7 for 80/100

--- C#/C# 2/3. Methods-HW/First-Larger-Than-Neighbours/IndexOfTheFirstElementBiggerThanItsNeighbours.cs
+++ C#/C# 2/3. Methods-HW/First-Larger-Than-Neighbours/IndexOfTheFirstElementBiggerThanItsNeighbours.cs
@@ -1,8 +1,15 @@
-﻿/*  Problem 6. First larger than neighbours
-    Write a method that returns the index of the first element in array 
-    that is larger than its neighbours, or -1, if there’s no such element.
-    Use the method from the previous exercise.
- */
+﻿/*  6. First larger than neighbours
+    Write a method that returns the index of the first element in array that is larger than its neighbours, 
+    or -1, if there is no such element.
+    Input:
+            On the first line you will receive the number N - the size of the array
+            On the second line you will receive N numbers sepated by spaces - the array
+    Output: Print the index of the first element that is larger than its neighbours or -1 if none are
+    Constraints: 1 <= N <= 1024
+    Sample tests:
+    Input 	                    Output
+    6
+    -26 -25 -28 31 2 27 	    1           */
 
 using System;
 
@@ -10,44 +17,41 @@ class IndexOfTheFirstElementBiggerThanItsNeighbours
 {
     static void Main()
     {
-        int[] array = ArrayInput();
-
-        for (int i = 1; i < array.Length - 1; i++)  // the first and the last elements do not satisfy the condition - no need to check
-        {
-            if (ElementCompararer(array, i) != -1)
-            {
-                return;
-            }
-        }
-
-        Console.WriteLine("-1 (there’s no such element)");
-    }
-
-    public static int[] ArrayInput()
-    {
-        Console.Write("Enter array length: ");
         int length = int.Parse(Console.ReadLine());
 
         int[] array = new int[length];
+        ArrayInput(length, array);
+
+        Console.WriteLine(FirstLargerThanNeighbours(length, array));
+    }
+
+    static void ArrayInput(int length, int[] array)
+    {
+        string[] numbersAsText = Console.ReadLine()
+            .Split(new char[] { ' ', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
 
         for (int i = 0; i < length; i++)
         {
-            Console.Write("element[{0}] = ", i);
-            array[i] = int.Parse(Console.ReadLine());
+            array[i] = int.Parse(numbersAsText[i]);
         }
-        return array;
     }
 
-    static int ElementCompararer(int[] array, int elemPosition)
+    static int FirstLargerThanNeighbours(int length, int[] array)
     {
-        if ((array[elemPosition] > array[elemPosition - 1]) && (array[elemPosition] > array[elemPosition + 1]))
+        int index = -1;
+
+        for (int i = 1; i < length - 1; i++)
         {
-            Console.WriteLine("\nThe index of the first element in the array\n that is larger than its neighbours is: {0}\n", elemPosition);
-            return elemPosition;
+            if (array[i] > array[i - 1] && array[i] > array[i + 1])
+            {
+                return i;
+                // other way
+                //index = i;
+                //break;
+            }
         }
-        else
-        {
-            return -1;
-        }
+
+        return index;
     }
+
 }
--- C#/C# 2/3. Methods-HW/Reverse-Number/DigitsReverse.cs
+++ C#/C# 2/3. Methods-HW/Reverse-Number/DigitsReverse.cs
@@ -1,14 +1,13 @@
-﻿/*  Problem 7. Reverse number
-    Write a method that reverses the digits of given decimal number.
+﻿/*  7. Reverse number
+    Write a method that reverses the digits of a given decimal number.
     Example:
                 input 	    output
                 256 	    652
-                123.45 	    54.321
- */
+                123.45 	    54.321              */
 
 using System;
 using System.Threading;
-using System.Globalization; 
+using System.Globalization;
 
 class DigitsReverse
 {
@@ -16,13 +15,7 @@ class DigitsReverse
     {
         Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
 
-        Console.Write("Enter a decimal number: ");
-        double number;  // the type is double because of the second example
-        while (!double.TryParse(Console.ReadLine(), out number))
-        {            
-            Console.WriteLine("Invalid number.");
-            Console.Write("Please enter a correct decimal number: ");        
-        }
+        double number = double.Parse(Console.ReadLine());
 
         Console.WriteLine(DigitsReverser(number));
     }
@@ -31,14 +24,21 @@ class DigitsReverse
     {
         string input = number.ToString();
 
-        string output = null;
+        string reversedNum = null;
 
-        for (int i = 0; i < input.Length; i++)
+        // first way
+        for (int i = input.Length - 1; i >= 0; i--)
         {
-            output = output + input[input.Length - 1 - i];
+            reversedNum += input[i];
         }
 
-        return double.Parse(output);
+        // other way
+        //for (int i = 0; i < input.Length; i++)
+        //{
+        //    reversedNum += input[input.Length - 1 - i];
+        //}
+
+        return double.Parse(reversedNum);
     }
 
 }
