25b6b4edce6e829b05e2184499d22c6935906a77
petyakostova <petya_kostova_@abv.bg>
2016-05-18 14:42:06 +0300

    modify Number-As-Array/NumbersAsArraysSum.cs

--- C#/C# 2/3. Methods-HW/Number-As-Array/NumbersAsArraysSum.cs
+++ C#/C# 2/3. Methods-HW/Number-As-Array/NumbersAsArraysSum.cs
@@ -1,8 +1,23 @@
-﻿/*  Problem 8. Number as array
-    Write a method that adds two positive positive numbers represented as arrays of digits 
-    (each array element arr[i] contains a digit; the last digit is kept in arr[0]).
-    Each of the numbers that will be added could have up to 10 000 digits.
- */
+﻿/*  8. Number as array
+    Write a method that adds two positive integer numbers represented as arrays of digits 
+    (each array element arr[i] contains a digit; the last digit is kept in arr[0]). 
+    Write a program that reads two arrays representing positive integers and outputs their sum.
+    Input:
+    On the first line you will receive two numbers separated by spaces - the size of each array
+    On the second line you will receive the first array
+    On the third line you will receive the second array
+    Output: Print the sum as an array of digits (as described)
+            Digits should be separated by spaces
+    Constraints: Each of the numbers that will be added could have up to 10 000 digits.
+    Sample tests:
+                    Input 	        Output
+                    3 4
+                    8 3 3
+                    6 2 4 2 	    4 6 7 2
+
+                    5 5
+                    0 3 9 3 2
+                    5 9 5 1 7 	    5 2 5 5 9                            */
 
 using System;
 using System.Collections.Generic;
