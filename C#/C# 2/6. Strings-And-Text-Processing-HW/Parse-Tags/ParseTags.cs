eabcc90dd9cfd6d229efdea69997cfcf655769f4
petyakostova <petya_kostova_@abv.bg>
2016-05-21 20:44:28 +0300

    solve Parse-Tags/ParseTags.cs

--- C#/C# 2/6. Strings-And-Text-Processing-HW/Parse-Tags/ParseTags.cs
+++ C#/C# 2/6. Strings-And-Text-Processing-HW/Parse-Tags/ParseTags.cs
@@ -1,24 +1,25 @@
-﻿/*  Problem 5. Parse tags
-    You are given a text. Write a program that changes the text in all regions surrounded by the tags <upcase> and </upcase> to upper-case.
-    The tags cannot be nested.
-    Example: 
-    We are living in a <upcase>yellow submarine</upcase>. We don't have <upcase>anything</upcase> else.
-    The expected result: We are living in a YELLOW SUBMARINE. We don't have ANYTHING else.
+﻿/*  5. Parse tags
+    You are given a text. Write a program that changes the text in all regions 
+    surrounded by the tags <upcase> and </upcase> to upper-case.
+    Input: On the only line you will receive a string - the text
+    Output: Print the changed string on one line
+    Constraints: The tags will not be nested. String length will be <= 10000
+    Sample tests:
+    Input 	
+    We are living in a <upcase>yellow submarine</upcase>. We don't have <upcase>anything</upcase> else. 	
+    Output
+    We are living in a YELLOW SUBMARINE. We don't have ANYTHING else.
  */
 
 using System;
 using System.Text;
-using System.Text.RegularExpressions;
+//using System.Text.RegularExpressions;
 
 class ParseTags
 {
     static void Main()
     {
-        // for easier testing
-        string text = "We are living in a <upcase>yellow submarine</upcase>. We don't have <upcase>anything</upcase> else.";
-
-        //// the real input
-        //string str = Console.ReadLine();
+        string text = Console.ReadLine();
 
         StringBuilder sb = new StringBuilder();
 
@@ -30,6 +31,7 @@ class ParseTags
             {
                 i++;
                 toUpper = toUpper * (-1); // an opening tag => start UPPER
+
                 while (text[i] != '>')    // disregard all of the tag's content
                 {
                     i++;
@@ -40,7 +42,7 @@ class ParseTags
                 if (toUpper == 1)
                 {
                     sb.Append(text[i].ToString().ToUpper());
-                }                                              
+                }
                 else
                 {
                     sb.Append(text[i]);
@@ -50,7 +52,8 @@ class ParseTags
         }
         Console.WriteLine(sb.ToString());
 
-        ////other way
-        //Console.WriteLine(Regex.Replace(str, "<upcase>(.*?)</upcase>", word => word.Groups[1].Value.ToUpper()));
+        //other way
+        //Console.WriteLine(Regex
+        //    .Replace(text, "<upcase>(.*?)</upcase>", word => word.Groups[1].Value.ToUpper()));
     }
 }
