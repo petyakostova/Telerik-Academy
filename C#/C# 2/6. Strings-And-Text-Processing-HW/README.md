d22e335c714dfb00b264b465740b981c7f220cd4
petyakostova <petya_kostova_@abv.bg>
2016-05-23 01:57:20 +0300

    modify Unicode-Characters/UnicodeCharacters.cs for 60/100

--- C#/C# 2/6. Strings-And-Text-Processing-HW/README.md
+++ C#/C# 2/6. Strings-And-Text-Processing-HW/README.md
@@ -13,7 +13,7 @@ Homework: Processing
 #### 7. [Encode/decode](https://github.com/petyakostova/Telerik-Academy/tree/master/C%23/C%23%202/6.%20Strings-And-Text-Processing-HW/Encode-Decode)
 #### 8. [Extract sentences 60/100](https://github.com/petyakostova/Telerik-Academy/tree/master/C%23/C%23%202/6.%20Strings-And-Text-Processing-HW/Extract-Sentences)
 #### 9. [Forbidden words](https://github.com/petyakostova/Telerik-Academy/tree/master/C%23/C%23%202/6.%20Strings-And-Text-Processing-HW/Forbidden-Words)
-#### 10. [Unicode characters]
+#### 10. [Unicode characters 60/100](https://github.com/petyakostova/Telerik-Academy/tree/master/C%23/C%23%202/6.%20Strings-And-Text-Processing-HW/Unicode-Characters)
 #### 11. [Format number]
 #### 12. [Parse URL]
 #### 13. [Reverse sentence]
--- C#/C# 2/6. Strings-And-Text-Processing-HW/Unicode-Characters/UnicodeCharacters.cs
+++ C#/C# 2/6. Strings-And-Text-Processing-HW/Unicode-Characters/UnicodeCharacters.cs
@@ -1,10 +1,13 @@
-﻿/*  Problem 10. Unicode characters
+﻿/*  10. Unicode characters
     Write a program that converts a string to a sequence of C# Unicode character literals.
-    Use format strings.
-    Example:
-    input 	output
-    Hi! 	\u0048\u0069\u0021
+    Input: On the only input line you will receive a string
+    Output: Print the string in C# Unicode character literals on a single line
+    Sample tests:
+                    Input 	    Output
+                    Hi! 	    \u0048\u0069\u0021
  */
+
+// 60/100 in bgcoder
 
 using System;
 using System.Text;
@@ -16,11 +19,18 @@ class UnicodeCharacters
         string input = Console.ReadLine();
 
         StringBuilder answer = new StringBuilder();
-
+        
         for (int i = 0; i < input.Length; i++)
         {
             answer.AppendFormat("\\u{0:X4}", (int)input[i]);
+            //answer.Append(string.Format("\\u{0:X4}", (int)input[i]));
         }
+
+        // other way
+        //foreach (char character in input)
+        //{
+        //    answer.AppendFormat("\\u{0:X4}", (int)character);
+        //}
 
         Console.WriteLine(answer.ToString());
     }
