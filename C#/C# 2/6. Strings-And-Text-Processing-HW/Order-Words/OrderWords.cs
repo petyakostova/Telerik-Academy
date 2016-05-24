644386d2eed57056f72bcf2fea73e7d7e766c261
petyakostova <petya_kostova_@abv.bg>
2016-05-24 14:30:40 +0300

    solve Order-Words/OrderWords.cs

--- C#/C# 2/6. Strings-And-Text-Processing-HW/Order-Words/OrderWords.cs
+++ C#/C# 2/6. Strings-And-Text-Processing-HW/Order-Words/OrderWords.cs
@@ -1,9 +1,9 @@
-﻿/*  Problem 24. Order words
+﻿/*  24. Order words
     Write a program that reads a list of words, separated by spaces and prints the list in an alphabetical order.
  */
 
 using System;
-using System.Linq;  // needed for OrderBy and Select
+using System.Linq;  // needed for OrderBy() and Select()
 
 class OrderWords
 {
@@ -14,15 +14,20 @@ class OrderWords
         string[] words = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
         // first way
-        var ordered = words.OrderBy(x => x);
-        Console.WriteLine("\nWords sorted in alphabetical order:");
-        foreach (var item in ordered)
-        {
-            Console.WriteLine(item);
-        }
-        
-        //// second way
+
+        //var ordered = words.OrderBy(x => x);
+
+        //Console.WriteLine("\nWords sorted in alphabetical order:");
+
+        //foreach (var item in ordered)
+        //{
+        //    Console.WriteLine(item);
+        //}
+
+        // second way
+
         //Array.Sort(words);
+
         //Console.WriteLine("\nWords sorted in alphabetical order:\n{0}\n",
         //    string.Join("\n", words.Select(x => string.Format("=> {0}", x))));
 
