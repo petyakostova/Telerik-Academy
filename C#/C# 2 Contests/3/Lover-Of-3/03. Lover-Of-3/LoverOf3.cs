2b35fbc4fbde4512745864604a872d7dbfdaef5c
petyakostova <petya_kostova_@abv.bg>
2016-05-28 18:15:03 +0300

    update README.md and Lover-Of-3/03. Lover-Of-3/3. Lover of 3_Description.doc

--- C#/C# 2 Contests/3/Lover-Of-3/03. Lover-Of-3/LoverOf3.cs
+++ C#/C# 2 Contests/3/Lover-Of-3/03. Lover-Of-3/LoverOf3.cs
@@ -4,27 +4,30 @@
     using System.Linq;
 
     class LoverOf3
-    {
-        // 1. read field sizes and create boolean field
-        // 2. read directions
+    {              
         // 2.1 go in that direction until possible
         // 2.2 sum cells on the way using the formula
         // 2.2.2 keep information if we have already collected this cell
-        // 2.3 read another direction
-        // 3. print the result
+        // 2.3 read another direction        
 
         static void Main()
         {
+            /* instead of creating int matrix and fill it, just use formula to calculate the current cell's value
+               and create boolean matrix with false-s (still not visited cells)*/
+
+            // 1. read field sizes and create boolean field
             var fieldSizes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 
+            // 2. read directions
             var rowsCount = fieldSizes[0];
             var colsCount = fieldSizes[1];
 
+            // the boolean matrix will be initialize with the default bool value => false
             var field = new bool[rowsCount, colsCount];
 
-            var n = int.Parse(Console.ReadLine());
+            var n = int.Parse(Console.ReadLine()); // the number of directions
 
-            long result = 0;
+            long sum = 0;
 
             int row = rowsCount - 1;
             int col = 0;
@@ -32,10 +35,10 @@
 
             for (int i = 0; i < n; i++)
             {
-                var move = Console.ReadLine().Split(' ');
+                string[] move = Console.ReadLine().Split(' ');
 
                 var direction = move[0];
-                var repeats = int.Parse(move[1]);
+                int repeats = int.Parse(move[1]); // how much to move to that direction
 
                 var deltaRow = direction.Contains("U") ? -1 : 1;
                 var deltaCow = direction.Contains("L") ? -1 : 1;
@@ -61,7 +64,7 @@
                         // if not collected
                         if (!field[row, col])
                         {
-                            result += currentCell;
+                            sum += currentCell;
                             field[row, col] = true;
                         }
                     }
@@ -73,7 +76,8 @@
                 }
             }
 
-            Console.WriteLine(result);
+            // 3. print the result
+            Console.WriteLine(sum);
         }
 
         static bool IsInside(int row, int col, bool[,] matrix)
