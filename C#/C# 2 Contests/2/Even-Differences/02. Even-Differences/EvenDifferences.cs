d0883b00fa19d15140cdc1982bf63b640d89f2dd
petyakostova <petya_kostova_@abv.bg>
2016-05-28 16:31:29 +0300

    solve 2/Even-Differences-AS/EvenDifferencesAS.cs

---
+++ C#/C# 2 Contests/2/Even-Differences/02. Even-Differences/EvenDifferences.cs
@@ -0,0 +1,46 @@
+﻿namespace _02.Even_Differences
+{
+    using System;
+    using System.Linq;
+
+    class EvenDifferences
+    {
+        static void Main()
+        {
+            // the biggest abs diff is 2 000 000 000 - (-2 000 000 000) = 4 000 000 000 => long
+
+            // 1. read input
+            var sequence = Console.ReadLine()
+                .Split(' ')
+                .Select(long.Parse)
+                .ToArray(); // 2. put the input numbers in an array
+
+            long sum = 0;
+            int i = 1;
+            // 3. iterate over the array and calculate the sum
+            while (i < sequence.Length)
+            {
+                // 3.1 find absolute difference of current and previous
+                long absDiff = Math.Abs(sequence[i] - sequence[i - 1]);
+
+                // 3.2 if even difference, sum it
+                if (absDiff % 2 == 0)
+                {
+                    // sum even abs difference
+                    sum += absDiff;
+                    // 3.3 make even jump
+                    i += 2;
+                }
+                else // if odd
+                {
+                    // 3.3 make odd jump
+                    i++;
+                }
+            }
+
+            // 4. print the sum
+            Console.WriteLine(sum);
+
+        }
+    }
+}
---
+++ C#/C# 2 Contests/2/Even-Differences/Even-Differences-AS/EvenDifferencesAS.cs
@@ -0,0 +1,43 @@
+﻿namespace Even_Differences_AS
+{
+    using System;
+
+    class EvenDifferencesAS
+    {
+        public static void Main()
+        {
+            var line = Console.ReadLine();
+            var numbersAsString = line.Split(' ');
+
+            var numbers = new long[numbersAsString.Length];
+
+            for (int j = 0; j < numbersAsString.Length; j++)
+            {
+                numbers[j] = int.Parse(numbersAsString[j]);
+            }
+
+            var answer = SumOfDifferences(numbers);
+
+            Console.WriteLine(answer);
+        }
+
+        private static long SumOfDifferences(long[] numbers)
+        {
+            long sum = 0;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                long diff = Math.Abs(numbers[i] - numbers[i - 1]);
+
+                if (diff % 2 == 0)
+                {
+                    i++;
+                    sum += diff;
+                }
+            }
+
+            return sum;
+        }
+
+    }
+}
