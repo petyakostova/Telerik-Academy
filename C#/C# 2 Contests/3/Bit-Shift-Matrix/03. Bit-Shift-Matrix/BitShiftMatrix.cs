b7c68c589465f5b260e1353f2120d2cf0b65d846
petyakostova <petya_kostova_@abv.bg>
2016-05-28 16:55:10 +0300

    modify Lover-Of-3/03. Lover-Of-3/LoverOf3.cs and Bit-Shift-Matrix/03. Bit-Shift-Matrix/BitShiftMatrix.cs

--- C#/C# 2 Contests/3/Bit-Shift-Matrix/03. Bit-Shift-Matrix/BitShiftMatrix.cs
+++ C#/C# 2 Contests/3/Bit-Shift-Matrix/03. Bit-Shift-Matrix/BitShiftMatrix.cs
@@ -1,13 +1,98 @@
 ﻿namespace _03.Bit_Shift_Matrix
 {
     using System;
+    using System.Linq;
+    using System.Numerics;
 
     class BitShiftMatrix
     {
+        // 1. read input
+        // 2. declare a field/matrix with R and C
+        // 3. for every move
+        // 3.1 calcute move coordinates
+        // 3.2 go to column and collect everything on the way and mark it as collected
+        // 3.3 go to row and collect everything on the way and mark it as collected
+        // 3.4 do the same for all moves
+        // 4. print the resulting sum
+
         static void Main()
         {
+            var rowsCount = int.Parse(Console.ReadLine());
+            var colsCount = int.Parse(Console.ReadLine());
 
+            var n = Console.ReadLine();
 
+            var collected = new bool[rowsCount, colsCount];
+
+            var moves = Console.ReadLine()
+                                    .Split(' ')
+                                    .Select(int.Parse)
+                                    .ToArray();
+
+            BigInteger sum = 0;
+            var row = rowsCount - 1;
+            var col = 0;
+            BigInteger currentCellValue = 1;
+            var coeff = Math.Max(rowsCount, colsCount);
+
+            foreach (var move in moves)
+            {
+                var nextRow = move / coeff;
+                var nextCol = move % coeff;
+
+                var deltaCol = col > nextCol ? -1 : 1;
+
+                while (col != nextCol)
+                {
+                    if (!collected[row, col])
+                    {
+                        sum += currentCellValue;
+                        collected[row, col] = true;
+                    }
+
+                    if (deltaCol == 1)
+                    {
+                        currentCellValue *= 2;
+                    }
+                    else
+                    {
+                        currentCellValue /= 2;
+                    }
+
+                    col += deltaCol;
+                }
+
+                var deltaRow = row > nextRow ? -1 : 1;
+
+                while (row != nextRow)
+                {
+                    if (!collected[row, col])
+                    {
+                        sum += currentCellValue;
+                        collected[row, col] = true;
+                    }
+
+                    if (deltaRow == 1)
+                    {
+                        currentCellValue /= 2;
+                    }
+                    else
+                    {
+                        currentCellValue *= 2;
+                    }
+
+                    row += deltaRow;
+                }
+            }
+
+            if (!collected[row, col])
+            {
+                sum += currentCellValue;
+                collected[row, col] = true;
+            }
+
+            Console.WriteLine(sum);
         }
+
     }
 }
--- C#/C# 2 Contests/3/Lover-Of-3/03. Lover-Of-3/LoverOf3.cs
+++ C#/C# 2 Contests/3/Lover-Of-3/03. Lover-Of-3/LoverOf3.cs
@@ -1,13 +1,88 @@
 ﻿namespace _03.Lover_Of_3
 {
     using System;
+    using System.Linq;
 
     class LoverOf3
     {
+        // 1. read field sizes and create boolean field
+        // 2. read directions
+        // 2.1 go in that direction until possible
+        // 2.2 sum cells on the way using the formula
+        // 2.2.2 keep information if we have already collected this cell
+        // 2.3 read another direction
+        // 3. print the result
+
         static void Main()
         {
+            var fieldSizes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 
+            var rowsCount = fieldSizes[0];
+            var colsCount = fieldSizes[1];
 
+            var field = new bool[rowsCount, colsCount];
+
+            var n = int.Parse(Console.ReadLine());
+
+            long result = 0;
+
+            int row = rowsCount - 1;
+            int col = 0;
+            int currentCell = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var move = Console.ReadLine().Split(' ');
+
+                var direction = move[0];
+                var repeats = int.Parse(move[1]);
+
+                var deltaRow = direction.Contains("U") ? -1 : 1;
+                var deltaCow = direction.Contains("L") ? -1 : 1;
+
+                // could also be from 0 to repeats - 1
+                for (int j = 1; j < repeats; j++)
+                {
+                    if (IsInside(row + deltaRow, col + deltaCow, field))
+                    {
+                        // go to the cell
+                        row += deltaRow;
+                        col += deltaCow;
+
+                        if (deltaRow == -1 && deltaCow == 1)
+                        {
+                            currentCell += 6;
+                        }
+                        else if (deltaRow == 1 && deltaCow == -1)
+                        {
+                            currentCell -= 6;
+                        }
+
+                        // if not collected
+                        if (!field[row, col])
+                        {
+                            result += currentCell;
+                            field[row, col] = true;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+            }
+
+            Console.WriteLine(result);
         }
+
+        static bool IsInside(int row, int col, bool[,] matrix)
+        {
+            bool rowIsInField = 0 <= row && row < matrix.GetLength(0);
+            bool colIsInField = 0 <= col && col < matrix.GetLength(1);
+
+            return rowIsInField && colIsInField;
+        }
+
     }
 }
