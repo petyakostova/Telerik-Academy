3af3ad8573a60ad8679f90804a38ca959364fa95
petyakostova <petya_kostova_@abv.bg>
2016-05-30 23:32:44 +0300

    solve Patterns/03. Patterns/Patterns.cs

--- C#/C# 2 Contests/3/Patterns/03. Patterns/Patterns.cs
+++ C#/C# 2 Contests/3/Patterns/03. Patterns/Patterns.cs
@@ -69,6 +69,7 @@
                         }
 
                         maxSum = Math.Max(maxSum, sum);
+
                         // other way
                         //if (sum > maxSum)
                         //{
