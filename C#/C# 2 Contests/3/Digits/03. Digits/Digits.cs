ae42b2212c207ee855e79ef557b921c54ae3fd39
petyakostova <petya_kostova_@abv.bg>
2016-05-31 00:13:32 +0300

    solve Digits/03. Digits/Digits.cs for 92-96/100 in bgcoder => 1-2 time limits

--- C#/C# 2 Contests/3/Digits/03. Digits/Digits.cs
+++ C#/C# 2 Contests/3/Digits/03. Digits/Digits.cs
@@ -1,47 +1,54 @@
-﻿namespace _03.Digits
+﻿// 92-96/100 in bgcoder => 1-2 time limits
+namespace _03.Digits
 {
     using System;
     using System.Collections.Generic; // List<bool
 
     class Digits
     {
-        static int[,] digits;   // така може да се достъпва по всички методи в този клас
+        static int[,] digits;   // accessible from all the methods in the class
 
         static void Main()
         {
-            int n = int.Parse(Console.ReadLine());
+            // list with all the patterns from 0 to 9
+            List<bool[,]> patterns = InitializeListOfPatterns();
 
-            var patterns = InitializeListOfPatterns();
+            // Input
+            int n = int.Parse(Console.ReadLine());
 
             digits = new int[n, n];
 
             for (int row = 0; row < n; row++)
             {
                 string[] currentLine = Console.ReadLine().Split(' ');
+
                 for (int col = 0; col < currentLine.Length; col++)
                 {
                     digits[row, col] = int.Parse(currentLine[col]);
                 }
             }
 
+            // Calculating the sum
             int sum = 0;
 
             for (int row = 0; row <= n - 5; row++)
             {
                 for (int col = 0; col <= n - 3; col++)
                 {
+                    // the beginning of the one (most up right)
                     if (digits[row + 2, col] == 1)
                     {
-                        if (CheckPattern(patterns[1], // кой pattern ни трябва
-                            1, // кой digit
+                        if (CheckPattern(patterns[1], // whose pattern we need
+                            1, // which digit
                             row,
                             col))
                         {
                             sum += 1;
-                            continue;  // спира текущата итерация на най-вътрешния цикъл без да излиза от него
+                            continue;  // stops the current iteration on the inner most loop without exit the loop
                         }
                     }
 
+                    // the beginning of the two
                     if (digits[row + 1, col] == 2)
                     {
                         if (CheckPattern(patterns[2], 2, row, col))
@@ -51,6 +58,7 @@
                         }
                     }
 
+                    // the beginning of the 3-9
                     int currentDigit = digits[row, col];
 
                     if (CheckPattern(patterns[currentDigit], currentDigit, row, col))
@@ -60,17 +68,18 @@
                 }
             }
 
+            // Output
             Console.WriteLine(sum);
         }
 
-        static List<bool[,]> InitializeListOfPatterns()  // list bool-еви двумерни масиви
+        static List<bool[,]> InitializeListOfPatterns()  // returns list of boolean 2-dimentional arrays
         {
             var list = new List<bool[,]>();
 
             // the first element of the list returns the pattern of the zero
             list.Add(new bool[,]
                 {
-                    // faking zero;
+                    // faking zero; => no need to sum it
                 });
 
             // the second element of the list returns the pattern of the one
@@ -166,7 +175,7 @@
             return list;
         }
 
-        // от кой ред и коя колона да проверявам в digits дали имам такъв pattern => row, col
+        // from which row and which col to check in digits if it has such a pattern => row, col
         static bool CheckPattern(bool[,] pattern, int digit, int row, int col) 
         {
             for (int i = 0; i < pattern.GetLength(0); i++)
