e2611d36e00cfe6dc51a62823a50c19b085c6c34
petyakostova <petya_kostova_@abv.bg>
2016-06-24 17:07:17 +0300

    add Front-end/JS Fundamentals/conditional statements/06. Quadratic equation/quadratic-equation.js => 40/100

---
+++ Front-end/JS Fundamentals/conditional statements/06. Quadratic equation/quadratic-equation.js
@@ -0,0 +1,33 @@
+function solve(args) {
+    var a = +args[0],
+        b = +args[1],
+        c = +args[2],
+        x1,
+        x2;
+
+    // if (Discriminant < 0)
+    if (b * b - 4 * a * c < 0) {
+        console.log('no real roots');
+    } else {
+        // if (Discriminant === 0) 
+        if (b * b - 4 * a * c === 0) {
+            x1 = x2 = -b / (2 * a);
+            console.log('x1=x2=' + x1.toFixed(2));
+        } else { // if (Discriminant > 0)    
+            x1 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+            x2 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+
+            if (x1 < x2) {
+                console.log('x1=' + x1.toFixed(2) + '; x2=' + x2.toFixed(2));
+            } else {
+                console.log('x1=' + x2.toFixed(2) + '; x2=' + x1.toFixed(2));
+            }
+        }
+    }
+}
+
+solve(['2', '5', '-3']);
+solve(['-1', '3', '0']);
+solve(['-0.5', '4', '-8']);
+solve(['5', '2', '8']);
+solve(['0.2', '9.572', '0.2']);
