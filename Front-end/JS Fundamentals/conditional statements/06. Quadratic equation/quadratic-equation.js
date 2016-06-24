8a31f17818718386ad7b882244f02e0188bef556
petyakostova <petya_kostova_@abv.bg>
2016-06-24 17:42:10 +0300

    modify quadratic-equation.js

--- Front-end/JS Fundamentals/conditional statements/06. Quadratic equation/quadratic-equation.js
+++ Front-end/JS Fundamentals/conditional statements/06. Quadratic equation/quadratic-equation.js
@@ -1,3 +1,5 @@
+// 40/100 in bgcoder?
+
 function solve(args) {
     var a = +args[0],
         b = +args[1],
@@ -16,9 +18,14 @@ function solve(args) {
             x1 = (-b - Math.sqrt(D)) / (2 * a);
             x2 = (-b + Math.sqrt(D)) / (2 * a);
 
-            console.log('x1=' + x1.toFixed(2) + '; x2=' + x2.toFixed(2));
+            if (x1 < x2) {
+                console.log('x1=' + x1.toFixed(2) + '; x2=' + x2.toFixed(2));
+            } else {
+                console.log('x1=' + x2.toFixed(2) + '; x2=' + x1.toFixed(2));
+            }
         }
     }
+
 }
 
 solve(['2', '5', '-3']);
