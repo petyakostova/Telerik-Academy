d85d77d5cf6bdff376ff6a0c100da659106b4182
petyakostova <petya_kostova_@abv.bg>
2016-06-29 02:49:21 +0300

    solve Front-end/JS Fundamentals/arrays/02. Lexicographically comparison/lexicographically-compare.js

---
+++ Front-end/JS Fundamentals/arrays/02. Lexicographically comparison/lexicographically-compare.js
@@ -0,0 +1,17 @@
+function solve(args) {
+    var arr = args[0].split('\n'),
+        firstStr = arr[0],
+        secondStr = arr[1];
+
+    if (firstStr > secondStr) {
+        console.log('>');
+    } else if (firstStr < secondStr) {
+        console.log('<');
+    } else {              // firstStr === secondStr
+        console.log('=');
+    }
+
+}
+
+solve(['hello', 'halo']);
+solve(['food', 'food']);
