a8d4cd2ddb603e9b10c6ade2ce85f32cd50ddb14
petyakostova <petya_kostova_@abv.bg>
2016-06-29 15:36:32 +0300

    reformat code

--- Front-end/JS Fundamentals/functions/05. Larger than neighbours/larger-than-neighbours.js
+++ Front-end/JS Fundamentals/functions/05. Larger than neighbours/larger-than-neighbours.js
@@ -1,17 +1,15 @@
 function solve(args) {
     var i,
-    len = +args[0],
+        len = +args[0],
         numbers = args[1].split(' '),
         count;
 
-        console.log(LargerThanNeighboursCount(numbers, len));
-    
-    function LargerThanNeighboursCount(numbers, length){
+    console.log(LargerThanNeighboursCount(numbers, len));
+
+    function LargerThanNeighboursCount(numbers, length) {
         count = 0;
-        for (i = 1; i < length - 1; i+=1)
-        {
-            if (+numbers[i] > +numbers[i - 1] && +numbers[i] > +numbers[i + 1])
-            {
+        for (i = 1; i < length - 1; i += 1) {
+            if (+numbers[i] > +numbers[i - 1] && +numbers[i] > +numbers[i + 1]) {
                 count++;
             }
         }
