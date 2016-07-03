703f020c9fb17d737f78fc22ecf37dd7727feff4
petyakostova <petya_kostova_@abv.bg>
2016-07-03 17:44:41 +0300

    solve Front-end/JS Fundamentals Contests/1/1. Cakes/cakes.js and modify Cakes.docx

--- Front-end/JS Fundamentals Contests/1/1. Cakes/cakes.js
+++ Front-end/JS Fundamentals Contests/1/1. Cakes/cakes.js
@@ -1,25 +1,32 @@
 ﻿function solve(params) {
-  var count1,
-    count2,
-    count3,
-    price,
-    s = params[0],
-    c1 = params[1],
-    c2 = params[2],
-    c3 = params[3],
-    answer = 0;
+  var s = params[0],
+    c1 = params[1], c2 = params[2], c3 = params[3],
+    maxMoneyAmount = 0;
 
-  for (count1 = 0; count1 <= (s / c1) + 1; count1++) {
-    for (count2 = 0; count2 <= (s / c2) + 1; count2++) {
-      for (count3 = 0; count3 <= (s / c3) + 1; count3++) {
-        price = (count1 * c1) + (count2 * c2) + (count3 * c3);
+  for (var count1 = 0; count1 < s / c1 + 1; count1 += 1) {
+    for (var count2 = 0; count2 < s / c2 + 1; count2 += 1) {
+      for (var count3 = 0; count3 < s / c3 + 1; count3 += 1) {
+
+        var price = (count1 * c1) + (count2 * c2) + (count3 * c3);
+
         if (price <= s) {
-          // if (price >= answer) console.log(s - price, i1, i2, i3);
-          answer = Math.max(answer, price);
+          maxMoneyAmount = Math.max(maxMoneyAmount, price);
+        } else { // for optimization
+          break;
         }
+
       }
     }
   }
 
-  return answer;
+  console.log(maxMoneyAmount);
+  //return maxMoneyAmount;
 }
+
+solve([110, 13, 15, 17]); // 110
+solve([20, 11, 200, 300]); // 11
+solve([110, 19, 29, 39]); // 107
+
+//console.log(solve([110, 13, 15, 17]) == 110);
+//console.log(solve([20, 11, 200, 300]) == 11);
+//console.log(solve([110, 19, 29, 39]) == 107);
