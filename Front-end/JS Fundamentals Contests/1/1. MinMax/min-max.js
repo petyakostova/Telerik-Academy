02af968ca46b4a031b866fd7f24628f311a831b3
petyakostova <petya_kostova_@abv.bg>
2016-07-03 20:29:50 +0300

    add Front-end/JS Fundamentals Contests/1/1. MinMax/description and min-max.js

---
+++ Front-end/JS Fundamentals Contests/1/1. MinMax/min-max.js
@@ -0,0 +1,13 @@
+function solve(args) {
+    var n = parseInt(args[0]),
+        k = parseInt(args[1]),
+        numbersAsString = args[2];
+
+    
+
+    console.log();
+}
+
+solve(['4', '2', '1 3 1 8']); // 4,4,9
+solve(['5', '3', '7 7 8 9 10']); // 15,16,18
+solve(['8', '4', '1 8 8 4 2 9 8 11']); // 9,10,11,11,13
