bbcf424874642b9247de1af6f572537105675bb7
petyakostova <petya_kostova_@abv.bg>
2016-07-03 15:28:48 +0300

    solve 1. Sequences

---
+++ Front-end/JS Fundamentals Contests/1/1. Sequences/sequences-parseInt.js
@@ -0,0 +1,21 @@
+function solve(args) {
+	var n = parseInt(args[0]), // n = +args[0],
+		count = 1,
+		last = parseInt(args[1]);
+
+	for (var i = 2; i <= n; i++) {
+		var current = parseInt(args[i]);
+
+		if (current < last) {
+			count++;
+		}
+		
+		last = current;
+	}
+
+	console.log(count);
+}
+
+solve(['7', '1', '2', '-3', '4', '4', '0', '1']);
+solve(['6', '1', '3', '-5', '8', '7', '-6']);
+solve(['9', '1', '8', '8', '7', '6', '5', '7', '7', '6']);
--- Front-end/JS Fundamentals Contests/1/1. Sequences/sequences.js
+++ Front-end/JS Fundamentals Contests/1/1. Sequences/sequences.js
@@ -1,20 +1,20 @@
-function solve(input) {
-    //var count = parseInt(input[0]);
-    var result = 1;
+function solve(args) {
+    //var n = parseInt(args[0]); // n = args.length
+    var countIncrSeq = 1;
 
-    input = input.map(Number);  // преобразува входните данни от стринг в число
+    args = args.map(Number);  // converts the input data from string into a number
 
-    //console.log(input); // изписва редицата
+    //console.log(args); // print the sequence
 
-    for (var i = 2; i < input.length; i++) {
-        if (input[i - 1] > input[i]) {
-            result++;
+    for (var i = 2; i < args.length; i += 1) {
+        if (args[i - 1] > args[i]) {
+            countIncrSeq += 1;
         }
     }
 
-    console.log(result);
+    console.log(countIncrSeq);
 }
 
-solve([7,1,2,-3,4,4,0,1]);
-solve([6,1,3,-5,8,7,-6]);
-solve([9,1,8,8,7,6,5,7,7,6]);
+solve(['7', '1', '2', '-3', '4', '4', '0', '1']);
+solve(['6', '1', '3', '-5', '8', '7', '-6']);
+solve(['9', '1', '8', '8', '7', '6', '5', '7', '7', '6']);
