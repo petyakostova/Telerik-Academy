6df9c253ceb680f33d3a494d27516f4869ae5fd1
petyakostova <petya_kostova_@abv.bg>
2016-07-04 02:21:10 +0300

    solve horsy.js

--- Front-end/JS Fundamentals Contests/2/2. Horsy/horsy.js
+++ Front-end/JS Fundamentals Contests/2/2. Horsy/horsy.js
@@ -5,23 +5,23 @@ function solve(args) {
         return Math.pow(2, row) - col;
     }
     // testing the function getPoints()
-    //console.log(getPoints(0,0)); // 1
-    //console.log(getPoints(2,1)); // 3
-    //console.log(getPoints(1,4)); // -2
+    // console.log(getPoints(0, 0)); // 1
+    // console.log(getPoints(2, 1)); // 3
+    // console.log(getPoints(1, 4)); // -2
 
-    // ф-я, получаваща стринг и връща масив с редовете и колоните, к. имам
+    // ф-я, получаваща стринг, и връща масив с редовете и колоните, к. имам
     function getRowsAndCols(str) {
         var parts = str.split(' '); // разделяме по интервал
-        // и получаваме 2 подстринга - parts[0] и parts[1]
+        // и получаваме 2 подстринга => parts[0] и parts[1]
         // парсваме ги към цели числа
-        // и връщаме масив с 0лев елемент броя редове и 1-ви ел-т броя колони
+        // и връщаме масив с 0лев елемент = броя редове и 1-ви ел-т = броя колони
         return [parseInt(parts[0]), parseInt(parts[1])];
     }
     // testing the function getRowsAndCols()
-    //console.log(getRowsAndCols('3 5'));
-    //console.log(getRowsAndCols('123 1234'));
+    // console.log(getRowsAndCols('3 5'));
+    // console.log(getRowsAndCols('123 1234'));
 
-    // ф-я, к.да ми даде ст-та по rol и col от входните данни
+    // ф-я, к. да ми даде ст-та по rol и col от входните данни
     function getValue(args, row, col) {
         return args[row + 1][col];
     }
@@ -29,22 +29,22 @@ function solve(args) {
     //console.log(getValue(args, 0, 0)); // 5
     //console.log(getValue(args, 2, 4)); // 8    
 
-    var rowsAndCols = getRowsAndCols(args[0]), // в args[0] се намира стринга, съдържащ редовете и колоните
-        rows = rowsAndCols[0],
-        cols = rowsAndCols[1],
+    var rowsAndCols = getRowsAndCols(args[0]), // в args[0] се намира стрингът, съдържащ:
+        rows = rowsAndCols[0],                 // редовете и 
+        cols = rowsAndCols[1],                 // колоните
 
-        // You start always at bottom-right cell [row-1, col-1].
+        // You start always at bottom-right cell [row-1, col-1]
         row = rows - 1, // текущият ред = бр.редове - 1
         col = cols - 1, // текущата колона = бр.колони - 1
 
         points = 0,
         moves = 0, // колко пъти сме стъпили върху дад.клетка
+
         used = []; // едномерен асоциативен масив с индекс "row col"
 
     // скоковете могат да се направят и с if-ове, но по-лесно е:
     // декларираме си в една променлива всички възможни индексирани от 0 движения на коня
-    // правим масив с 8 ел-та, всеки от които казва
-    // от тек.позиция колко местя реда и колоната
+    // правим масив с 8 ел-та, всеки от които казва от тек.позиция колко местя реда и колоната
     // 1=> 2 реда нагоре, 1 колона надясно; 2=> 1 ред нагоре, 2 колони надясно и т.н.
     var horseMoves = [[-2, 1], [-1, 2], [1, 2], [2, 1],
         [2, -1], [1, -2], [-1, -2], [-2, -1]]; // това са ми възможните движения
