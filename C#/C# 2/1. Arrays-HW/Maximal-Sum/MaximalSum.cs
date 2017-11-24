78314ecb5cad9ece78ad1a703a1b1832b417f2f0
petyakostova <petya_kostova_@abv.bg>
2017-11-24 12:31:33 +0200

    add task desription

--- C#/C# 2/1. Arrays-HW/Maximal-Sum/MaximalSum.cs
+++ C#/C# 2/1. Arrays-HW/Maximal-Sum/MaximalSum.cs
@@ -1,10 +1,13 @@
 ﻿/*  Problem 8. Maximal sum
     Write a program that finds the maximal sum of consecutive elements in a given array of N numbers.
-    Can you do it with only one loop (with single scan through the elements of the array)?             */
+    Can you do it with only one loop (with single scan through the elements of the array)?             
+    Input: On the first line you will receive the number N. On the next N lines the numbers of the array will be given.
+    Output: Print the maximal sum of consecutive numbers.
+    Constraints: 1 <= N <= 1024         Time limit: 0.1s        Memory limit: 16MB      */
 
 using System;
 
-class MaximalSum
+public class MaximalSum
 {
     static void Main()
     {
